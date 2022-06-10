3ce1cb6682fcb5ad1bcb0ddbd32cf7e5e35d8d7d
varun21vaidya <varun21vaidya@gmail.com>
2022-06-11 03:34:59 +0530

    organized repo

--- ds dictionary.py
+++
@@ -1,6 +0,0 @@
-list1={'jan':2200, 'feb':2350, 'march':2600, 'April':2130,'may':2190}
-#getting values from keys
-print(list1['jan'])
-
-#getting keys from values
-print(list(list1.keys())[list(list1.values()).index(2600)])
