8aa94983b1c7e026d7daa2dcced2fc96cd6331b4
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-06-11 03:11:04 +0530

    Time: 45 ms (49.70%), Space: 13.8 MB (98.49%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,31 +1,33 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        
+        def realstr(s):
+            real=[]
+            for i in s:
+                if i!="#":
+                    real.append(i)
+                else:
+                    if real:
+                        real.pop()   
+            return real
+        
+        return realstr(s)==realstr(t)
+    
 #https://leetcode.com/problems/backspace-string-compare/
 #Runtime: 20 ms, faster than 99.27% of Python3 online submissions for Backspace String Compare.
 #Memory Usage: 14.1 MB, less than 93.90% of Python3 online submissions for Backspace String Compare.
 
 #(Asked in Amazon SDE1 interview)
 
-class Solution:
-    def soln(self, lst: str)-> list:
-        op=[]
-        for i in lst:
-            if i=="#" and op:
-               op.pop()
-            elif i!="#":
-                op.append(i)        
-        return op
+# class Solution:
+#     def soln(self, lst: str)-> list:
+#         op=[]
+#         for i in lst:
+#             if i=="#" and op:
+#                op.pop()
+#             elif i!="#":
+#                 op.append(i)        
+#         return op
     
-    def backspaceCompare(self, s: str, t: str) -> bool:
-        return self.soln(s)==self.soln(t)
-
-        
-#     def realstr(s):
-#         real=[]
-#         for i in s:
-#             if i!="#":
-#                 real.append(i)
-#             else:
-#                 if real:
-#                     real.pop()   
-#         return real
-
-#     return realstr(s)==realstr(t)
+#     def backspaceCompare(self, s: str, t: str) -> bool:
+#         return self.soln(s)==self.soln(t)
