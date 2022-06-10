aadb323556b1a5417ae8016d674f945163cd2de7
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-06-11 03:13:30 +0530

    Time: 34 ms (85.66%), Space: 13.8 MB (73.24%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,6 +1,8 @@
 #https://leetcode.com/problems/backspace-string-compare/
 #(Asked in Amazon SDE1 interview)
 
+# Runtime: 43 ms, faster than 55.97% of Python3 online submissions for Backspace String Compare.
+# Memory Usage: 13.8 MB, less than 98.49% of Python3 online submissions for Backspace String Compare.
 
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
@@ -18,6 +20,8 @@ class Solution:
         return realstr(s)==realstr(t)
     
 
+#BETTER SOLUTION    
+    
 #Runtime: 20 ms, faster than 99.27% of Python3 online submissions for Backspace String Compare.
 #Memory Usage: 14.1 MB, less than 93.90% of Python3 online submissions for Backspace String Compare.
 
