eb71f451cb5d8579409e253e0fab677fcc9d472b
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-06-26 04:08:24 +0530

    Time: 186 ms (58.39%), Space: 15.2 MB (79.03%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -6,6 +6,34 @@ class Solution:
         # return [i for i in range(ls-lp+1) if sorted(s[i:i+lp])==sortp]
         
         
+        
+        
+       # So one way was to calculate all anagrams of pat
+	   # and find those in txt
+	   # but calculting only anagram would be O(n!)
+	    
+	    
+	   # Approach: Sliding Window
+	   # As there is string, substring and fixed window(len of substring)
+	   # we can use sliding window for this
+	    
+	   # so parts of sliding window:
+	   # initialization, while loop, calculation part, condition and ans part
+	    
+	   # so here for each sliding window we can check which letters are present
+	   # and are the number of each letters(count) same for window and pat
+	   # if yes to both then its anagram of pat
+	    
+	   # so if we are considering count of strings we will use dictionary
+	    
+	   # so first we will create counted dictionary of pat
+	   # now come to calulation part, for each sliding window we will 
+	   # either update count by 1 or if its not there create count with default 0 +1
+	    
+	   # and in the ans part, if both dicitonaries are same append index of i ie start to res
+	   # and with each slide drop arr[i] from dictionary value and if its 0 remove it
+        
+        
         #Approach Sliding Window
 	    #Initialization part
         i,j,res=0,0,[]
