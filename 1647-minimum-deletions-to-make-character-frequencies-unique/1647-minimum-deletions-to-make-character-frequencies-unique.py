b1becd5d1d7278d997700855c2319ec3ed33a318
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-06-28 13:05:45 +0530

    Time: 442 ms (18.07%), Space: 15 MB (17.36%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -5,13 +5,11 @@ class Solution:
             mapp[i]=mapp.get(i,0)+1
         print(mapp)
         freq=list(mapp.values())
-        notseen=[]
         print(freq)
         cnt=0
         for i in range(len(freq)):
-            print(i, freq[i])
-            if freq[i] in notseen:
-                while freq[i]-1 in notseen and freq[i]>=0:
+            if freq[i] in freq[:i]+freq[i+1:]:
+                while freq[i]-1 in freq and freq[i]>=0:
                     freq[i]-=1
                     cnt+=1
                     if freq[i]<=0:
@@ -19,8 +17,6 @@ class Solution:
                 if freq[i]>0:
                     freq[i]-=1
                     cnt+=1
-            notseen.append(freq[i])
-            print(freq)
         return cnt
             
         
