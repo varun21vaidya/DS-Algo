7e0a6e932615faf035e2e39142f4d5ba68e25719
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-06-28 13:08:36 +0530

    Time: 323 ms (36.64%), Space: 14.8 MB (51.90%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,3 +1,28 @@
+# class Solution:
+#     def minDeletions(self, s: str) -> int:
+#         mapp={}
+#         for i in s:
+#             mapp[i]=mapp.get(i,0)+1
+#         print(mapp)
+#         freq=list(mapp.values())
+#         notseen=[]
+#         print(freq)
+#         cnt=0
+#         for i in range(len(freq)):
+#             print(i, freq[i])
+#             if freq[i] in notseen:
+#                 while freq[i]-1 in notseen and freq[i]>=0:
+#                     freq[i]-=1
+#                     cnt+=1
+#                     if freq[i]<=0:
+#                         break
+#                 if freq[i]>0:
+#                     freq[i]-=1
+#                     cnt+=1
+#             notseen.append(freq[i])
+#             print(freq)
+#         return cnt
+
 class Solution:
     def minDeletions(self, s: str) -> int:
         mapp={}
@@ -5,11 +30,13 @@ class Solution:
             mapp[i]=mapp.get(i,0)+1
         # print(mapp)
         freq=list(mapp.values())
+        notseen=[]
         # print(freq)
         cnt=0
         for i in range(len(freq)):
-            if freq[i] in freq[:i]+freq[i+1:]:
-                while freq[i]-1 in freq and freq[i]>=0:
+            # print(i, freq[i])
+            if freq[i] in notseen:
+                while freq[i]-1 in notseen and freq[i]>=0:
                     freq[i]-=1
                     cnt+=1
                     if freq[i]<=0:
@@ -17,6 +44,9 @@ class Solution:
                 if freq[i]>0:
                     freq[i]-=1
                     cnt+=1
+            notseen.append(freq[i])
+            # print(freq)
         return cnt
-            
-        
+    
+    
+    
