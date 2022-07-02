8a65a78201329c7d7c4cd3cf10c5160889b744a9
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-07-03 00:03:21 +0530

    Time: 365 ms (79.83%), Space: 26.7 MB (77.43%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -9,9 +9,7 @@ class Solution:
         l=0
         b=0
         for i in range(len(hori)):
-            if(l<hori[i]-hori[i-1]):
-                l=hori[i]-hori[i-1]
+            l=max(l,hori[i]-hori[i-1])
         for j in range(len(ver)):
-            if (b<ver[j]-ver[j-1]):
-                b=ver[j]-ver[j-1]
+            b=max(b,ver[j]-ver[j-1])
         return l*b%(10**9 + 7)
