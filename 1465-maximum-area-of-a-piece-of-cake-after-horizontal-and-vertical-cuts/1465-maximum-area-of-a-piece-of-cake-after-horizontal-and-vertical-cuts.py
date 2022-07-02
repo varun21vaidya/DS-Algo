8626521a66439ef4cf6d86facb50da1025484b68
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-07-03 00:08:56 +0530

    Time: 424 ms (61.50%), Space: 26.7 MB (94.22%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -1,5 +1,16 @@
 class Solution:
     def maxArea(self, h: int, w: int, hori: List[int], ver: List[int]) -> int:
+        
+        
+#       so here we will assume 0 and h to be limitations for vertical height
+#       and 0 and w to be limitations for horizontal width
+#       so we will append them as cuts in respective cut arrays
+#       and then suppose array becomes (after sorting) for first eg h = 5, w = 4
+#       horizontalcuts= [0,1,2,4,5]
+#       verticalcuts = [0,1,3,4]
+#       now in each array we will calculate max diff to get max width and max height
+#       and then calculate area and take modulo of 10^9 + 7
+
         hori.append(0)
         hori.append(h)
         ver.append(0)
