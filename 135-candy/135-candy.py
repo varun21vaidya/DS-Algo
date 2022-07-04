f0b91eab78aa296346575c7b00e3ba8bfee62fc6
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-07-04 20:31:04 +0530

    Time: 299 ms (28.48%), Space: 17 MB (18.65%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,38 @@
+class Solution:
+    def candy(self, ratings):
+        count=0
+        # res=[1 for i in range(len(ratings))]
+        # changed=True
+        # while changed:
+        #     changed=False
+        #     for i in range(len(ratings)):
+        #         if i!=0 and ratings[i]>ratings[i-1] and res[i]<=res[i-1]:
+        #             res[i]=res[i-1]+1
+        #             changed=True
+        #         if i!=len(ratings)-1 and ratings[i]>ratings[i+1] and res[i]<=res[i+1]:
+        #             res[i]=res[i+1]+1
+        #             changed=True
+        # return sum(res)
+    
+        res,res2=[0 for i in range(len(ratings))],[1 for i in range(len(ratings))]
+        res[0]=1
+        res2[len(ratings)-1]=1
+        for i in range(1,len(ratings)):
+            if ratings[i]>ratings[i-1]:
+                res[i]=res[i-1]+1
+            else:
+                res[i]=1
+        print(res)
+
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                res2[i]=res2[i+1]+1
+            else:
+                res2[i]=1
+        print(res2)
+        
+        
+        for i in range(len(ratings)):
+            res[i]=max(res[i],res2[i])
+        print(res)
+        return sum(res)
