f8e651502ef46aaa85a557747e82ee1607eaaa70
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-07-07 00:34:18 +0530

    Time: 55 ms (44.92%), Space: 13.8 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -4,17 +4,20 @@ class Solution:
         #     return n
         # return self.fib(n-1)+self.fib(n-2)
         
-        # a,b=0,1
-        # if n<=1:
-        #     return n
-        # for _ in range(n-1):
-        #     a,b=b,a+b
-        # return b
+#       Space efficient Solution 
+        a,b=0,1
+        if n<=1:
+            return n
+        for _ in range(n-1):
+            a,b=b,a+b
+        return b
     
-        dp=[0,1]
-        for i in range(2,n+1):
-            dp.append(dp[i-1]+dp[i-2])
-        return dp[n]
+    
+#     DP solution (Memoisation)
+#         dp=[0,1]
+#         for i in range(2,n+1):
+#             dp.append(dp[i-1]+dp[i-2])
+#         return dp[n]
             
                 
                 
