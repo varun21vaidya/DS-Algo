e07bc9f0a52f167c7256b6619c335834120aba62
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-07-07 00:32:22 +0530

    Time: 53 ms (48.24%), Space: 13.8 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -3,12 +3,18 @@ class Solution:
         # if n<=1:
         #     return n
         # return self.fib(n-1)+self.fib(n-2)
-        a,b=0,1
-        if n<=1:
-            return n
-        for _ in range(n-1):
-            a,b=b,a+b
-        return b
+        
+        # a,b=0,1
+        # if n<=1:
+        #     return n
+        # for _ in range(n-1):
+        #     a,b=b,a+b
+        # return b
+    
+        dp=[0,1]
+        for i in range(2,n+1):
+            dp.append(dp[i-1]+dp[i-2])
+        return dp[n]
             
                 
                 
