90791f756335c70266d198fa00a35ff8d23b78a2
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-08-10 00:56:09 +0530

    Time: 157 ms (98.48%), Space: 14.3 MB (30.81%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -2,7 +2,16 @@ class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
         
 # first sort the array to get the numbers accordingly so we can take their mods with ease
-# then user 
+# then use a map to use it as counter of freq of each values and each occuring value 
+# will contribute to number of ways of binary tree
+# now iterate over each number 
+# to increase efficiency limit the loop upto square root of current num
+# binary tree consist of a value/node compared with each value upto that value
+# and if any of previous values can divide it to get int (factor A)
+# and that int value is also present in arr upto current num (factor B)
+# then num has both factor A and factor B as its leaf and it will be a binary tree
+# and if both are same then only one way else it has 2 ways contributing to total ways
+# as we move update the keys with ways and also the final ans regualrly with each iteration
 
         arr.sort()
         mapp, ans = dict(), 0
