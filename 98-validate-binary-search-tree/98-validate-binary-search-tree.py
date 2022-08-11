21b446bafe4f2dd0e506814636ef630c51b2d28b
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-08-12 01:00:05 +0530

    Time: 45 ms (95.05%), Space: 16.5 MB (79.64%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -6,6 +6,13 @@
 #         self.right = right
 class Solution:
     
+# Inorder traversals can be useful because we know that the inorder traversal of a BST
+# results in traversing the tree in an essentially increasing, sorted fashion. 
+# So essentially, the helper method is just performing this inorder traversal. 
+# so through each iteration, we keep updating the minn and maxx to contain root. 
+# But if we see that root is less than the root.left or root is greater than root.right
+# then we just break and return false
+    
     def helper(self,root, minn, maxx):
         if root==None: return True
         if (minn!=None and minn.val>=root.val): return False
