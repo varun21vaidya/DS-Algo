8e1a91523477d860fe7b4b4b7910e237911201e3
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-08-24 19:51:58 +0530

    Time: 188 ms (18.26%), Space: 13.8 MB (57.97%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,5 +1,10 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        # if n==3^x
+        # take log on both sides which will give log n == x log 3
+        # so x = log n/ log 3
+        # if x is integer return true
+        
         def log3(x):
             return math.log10(x)/math.log10(3)
         if n<=0: return False
