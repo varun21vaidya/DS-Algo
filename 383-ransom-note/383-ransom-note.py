5a705797d9a1d7e5a6bf18e125bbbf62fed5d301
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-08-25 20:10:04 +0530

    Time: 101 ms (41.46%), Space: 14.2 MB (53.79%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        m= Counter(magazine)
+        print(m)
+        count=0
+        for r in ransomNote:
+            if m[r]>0:
+                m[r]-=1
+                count+=1
+        # print(m, count)
+        if count==len(ransomNote): return True 
+        else: return False
