026ab3d0bb65c15e25cc5b2b31ab18ec2c2a2a76
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-02 22:07:04 +0530

    Time: 90 ms (33.24%), Space: 17.1 MB (15.89%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -8,54 +8,57 @@ class Solution:
     
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         
-#         def getheight(root):
-#             if root.left==None and root.right==None:
-#                 return 0
-#             left=0
-#             if root.left!=None:
-#                 left=getheight(root.left)
+        def getheight(root):
+            if root.left==None and root.right==None:
+                return 0
+            left=0
+            if root.left!=None:
+                left=getheight(root.left)
+            right=0    
+            if root.right!=None:
+                right=getheight(root.right)
                 
-#             if root.right!=None:
-#                 right=getheight(root.right)
-                
-#             return (max(left, right) +1)
-                   
-#         def calculate(node, level,ressum,resavg):
-#             # global ressum
-#             # global resavg
+            return (max(left, right) +1)
+               
             
-#             if node==None: return
-#             if ressum[level]==0:
-#                 levelnode=0
-#             ressum[level]+=node.val
-#             resavg[level]=ressum[level]/len(ressum)
-#             print(ressum,level,len(ressum))
-#             calculate(node.left, level+1,ressum,resavg)
-#             calculate(node.right, level+1,ressum,resavg)
+        ressum=[]
+        resavg=[]
+        levelcount=[]
+        def calculate(node, level,ressum,resavg,levelcount):
+            # global ressum
+            # global resavg
+            # global levelcount
+            if node==None: return
+            ressum[level]+=node.val
+            levelcount[level]+=1
+            resavg[level]=ressum[level]/levelcount[level]
+            # print(ressum,level,levelcount)
+            calculate(node.left, level+1,ressum,resavg,levelcount)
+            calculate(node.right, level+1,ressum,resavg,levelcount)
 
             
         
-#         level=getheight(root)+1
-#         ressum=[0]*level
-#         resavg=[0]*level
-#         # levelcount=0
-#         print(level)
-#         calculate(root, 0,ressum,resavg)
-#         return resavg
+        level=getheight(root)+1
+        ressum=[0]*level
+        resavg=[0]*level
+        levelcount=[0]*level
+        # print(level)
+        calculate(root, 0,ressum,resavg,levelcount)
+        return resavg
         
-        q=deque()
-        q.append(root)
-        ans =[]
-        while q:
-            qlen=len(q)
-            row =0
-            for i in range(qlen):
-                node = q.popleft()
-                row += node.val
-                if node.left: q.append(node.left)
-                if node.right: q.append(node.right)
-            ans.append(row/qlen)
-        return ans
+#         q=deque()
+#         q.append(root)
+#         ans =[]
+#         while q:
+#             qlen=len(q)
+#             row =0
+#             for i in range(qlen):
+#                 node = q.popleft()
+#                 row += node.val
+#                 if node.left: q.append(node.left)
+#                 if node.right: q.append(node.right)
+#             ans.append(row/qlen)
+#         return ans
         
         
         
