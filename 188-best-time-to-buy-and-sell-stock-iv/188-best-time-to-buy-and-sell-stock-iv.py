18768fb48e6ea00190dce87e79f126cc9bf0f25a
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-11 01:41:38 +0530

    Time: 173 ms (65.54%), Space: 13.7 MB (99.73%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -2,11 +2,12 @@ class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         
 #         # taking logic from Best Time to Buy and Sell Stock III
+#         https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/discuss/39611/Is-it-Best-Solution-with-O(n)-O(1)./242914
 #         # where we had to calculate for only 2 transactions
 #         # here we had to calculate for k transactions, so we made 2 arrays
 #         # which replaces firstbuysell which was used to track 1st transactions profit
 #         # and instead we use previous value from sell array to track that profit and
-#         # it will calculate max profits for k transactions from each iteration of prices
+#         # it will calculate max profits for k transactions for each iteration of prices
 
 
 #         firstbuy= float('inf')
@@ -17,9 +18,11 @@ class Solution:
 #             for i in range(k):
 #                 buy[i]=min(buy[i],stock-sell[i-1])
 #                 sell[i]=max(sell[i], stock-buy[i])
-#         # print(buy, sell)
 #         return sell[k-1]
         
+    
+    
+    
 #         #to improve timecomplexity
 #         #if k is greater than half of length of prices
 #         #ie greater than each transaction as n/2 means every transaction (buy&sell) is counted
@@ -27,16 +30,17 @@ class Solution:
 #         #so it just means its same as Best Time to Buy and Sell Stock II
         
 #         if k>=len(prices)//2:
-#             minimum_value=max(prices)
-#             profit, maximum_profit=0,0
-#             for i in prices:
-#                 minimum_value=min(minimum_value,i)
-#                 profit=i-minimum_value
-#                 maximum_profit=max(maximum_profit,profit)
-#             return maximum_profit
+#            profit=0
+#            for i in range(1,len(prices)):
+#                if prices[i]>prices[i-1]:
+#                    profit+=prices[i]-prices[i-1]
+#            return profit
 
 
 #this is same as explained above just rearranged
+#Time complexity, O(nk)
+#Space complexity, O(nk)
+
         if k>=len(prices)//2:
             profit=0
             for i in range(1,len(prices)):
@@ -52,7 +56,6 @@ class Solution:
             for i in range(k):
                 buy[i]=min(buy[i],stock-sell[i-1])
                 sell[i]=max(sell[i], stock-buy[i])
-        # print(buy, sell)
         return sell[k-1]
     
     
