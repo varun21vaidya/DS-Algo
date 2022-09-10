1ec7d939d47f1b3e7614eaea0bd3581c57e3e65e
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-11 01:18:00 +0530

    Time: 238 ms (43.03%), Space: 13.8 MB (94.99%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,5 +1,12 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        
+        # taking logic from Best Time to Buy and Sell Stock III
+        # where we had to calculate for only 2 transactions
+        # here we had to calculate for k transactions, so we made 2 arrays
+        # which replaces firstbuysell which was used to track 1st transactions profit
+        # and instead we use previous value from sell array to track that profit and
+        # it will calculate max profits for k transactions from each iteration of prices
         firstbuy= float('inf')
         firstbuysell=0
         buy=[firstbuy for i in range(k+1)]
@@ -8,5 +15,6 @@ class Solution:
             for i in range(k):
                 buy[i]=min(buy[i],stock-sell[i-1])
                 sell[i]=max(sell[i], stock-buy[i])
-        print(buy, sell)
+        # print(buy, sell)
         return sell[k-1]
+    
