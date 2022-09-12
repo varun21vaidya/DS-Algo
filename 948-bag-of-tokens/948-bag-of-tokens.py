f1ae63b5222f843919533f445028355581bd839f
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-12 20:21:06 +0530

    Time: 107 ms (22.17%), Space: 14.1 MB (7.08%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.py
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -3,24 +3,24 @@ class Solution:
         score=maxx=0
         i,j=0, len(tokens)-1
         tokens.sort()
-        print(tokens)
+        # print(tokens)
         while i<=j:
-            print(i,j)
+            # print(i,j)
             if i==len(tokens)-1 and tokens[i]>power:
                 return score
             elif tokens[i]<=power:
                 power-=tokens[i]
-                print("token",tokens[i], "power", power)
+                # print("token",tokens[i], "power", power)
                 i+=1
                 score+=1
             elif tokens[i]>power:
                 if score>0:
                     power+=tokens[j]
-                    print("token",tokens[j], "power", power)
+                    # print("token",tokens[j], "power", power)
                     j-=1
                     score-=1
                 else: return score
             maxx=max(maxx,score)
-            print(i,j,"score",score)
+            # print(i,j,"score",score)
         return 0 if maxx<0 else maxx
         
