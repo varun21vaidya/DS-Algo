253695e250449fc6ec9ea5d4177a58d274914a34
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-21 18:05:38 +0530

    Time: 78 ms (20.02%), Space: 14.1 MB (93.98%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,51 +1,38 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         
-#         # # TC: O(n*2^n) SC: O(n) #due to search in res 
+#         # # TC: O(n*2^n) SC: O(n) #extra time for conversion to tuple
 #         def helper(ind, arr, res, temp):
 #             if ind >= len(arr):
-#                 if temp not in res:
-#                     res.append(temp[:])
+#                 res.add(tuple(temp[:]))
 #                 return
 #             temp.append(arr[ind])
 #             helper(ind+1, arr, res, temp)
 #             temp.pop()
 #             helper(ind+1, arr, res, temp)
 
-#         ind, res, temp = 0, [], []
+#         ind, res, temp = 0, set(), []
 #         nums.sort()
 #         helper(ind, nums, res, temp)
 #         return res
-
-#         def helper(ind, arr, res, temp):
-#             if ind >= len(arr):
-#                 res.append(temp[:])
-#                 return
-            
-#             for i in range(ind,len(arr)):
-#                 if i>ind and arr[i]==arr[i-1]: continue
-                    
-#                 temp.append(arr[i])
-#                 helper(ind+1, arr, res, temp)
-#                 temp.pop()
-
-#         ind, res, temp = 0, [], []
-#         helper(ind, nums, res, temp)
-#         return res
-
-
-
-        # # TC: O(2^n) SC: O(n) 
+    
         def helper(ind, arr, res, temp):
-            if ind >= len(arr):
-                res.add(tuple(temp[:]))
-                return
-            temp.append(arr[ind])
-            helper(ind+1, arr, res, temp)
-            temp.pop()
-            helper(ind+1, arr, res, temp)
+            # when recursion is called temp is added to resultant array
+            res.append(temp[:])
+            
+            for i in range(ind,len(arr)):
+                if i!=ind and arr[i]==arr[i-1]: continue
+                    
+                temp.append(arr[i])
+                helper(i+1, arr, res, temp)
+                print(temp)
+                temp.pop()
 
-        ind, res, temp = 0, set(), []
         nums.sort()
+        ind, res, temp = 0, [], []
         helper(ind, nums, res, temp)
         return res
+
+
+
+    
