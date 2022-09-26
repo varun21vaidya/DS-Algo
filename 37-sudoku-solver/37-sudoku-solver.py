4ed88d5025c5a647ecdbd9ebd6811492cffa9093
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-26 20:36:06 +0530

    Time: 1749 ms (7.89%), Space: 13.8 MB (95.68%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.py
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -4,35 +4,31 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         
-        def isvalid(row,col,board,c):
-            c=str(c)
-            for i in range(9):
-                if board[i][col]==c: return False
-                if board[row][i]==c: return False
-                if board[3*(row//3)+ i//3][3*(col//3) + i%3]==c: return False
+#         def isvalid(row,col,board,c):
+#             c=str(c)
+#             for i in range(9):
+#                 if board[i][col]==c: return False
+#                 if board[row][i]==c: return False
+#                 if board[3*(row//3)+ i//3][3*(col//3) + i%3]==c: return False
                 
-            return True
+#             return True
         
-        for i in range(9):
-            for j in range(9):
-                if board[i][j]==".":
-                    for c in range(1,10):
-                        if isvalid(i,j,board,c):
-                            board[i][j]=str(c)                         
-                            if self.solveSudoku(board):
-                                return True
-                            else:
-                                board[i][j]="."
-                    return False
-        return True
-        
-        
+#         for i in range(9):
+#             for j in range(9):
+#                 if board[i][j]==".":
+#                     for c in range(1,10):
+#                         if isvalid(i,j,board,c):
+#                             board[i][j]=str(c)                         
+#                             if self.solveSudoku(board):
+#                                 return True
+#                             else:
+#                                 board[i][j]="."
+#                     return False
+#         return True
         
         
         
-        
-        
-        
+        # Step by Step Explanation: 
         
         def isvalid(row, col, board, c):
             # c is the number we want to check for this sudodku for that row,col position
