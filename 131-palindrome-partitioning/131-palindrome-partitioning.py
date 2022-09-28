51392b6a216a97406e8acd89e3faf7f016e8f1d3
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-28 20:19:25 +0530

    Time: 1688 ms (12.50%), Space: 28.9 MB (88.36%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,16 +1,19 @@
+
+    # first of all we need to partition the string 
+    # at each index and then check string upto that index is palindrome or not
+    # if its palindrome then use recursion and check the partition for remaining string
+    # with updated index 
+    # to update index we need to loop through the string and each point repeat the process
+    # ie check for palindrome and use recursion, and remember to remove that after recursion
+    # as backtracking process.
+    # now when the index goes out of string we will have the partitioned array    
+    # and append it to resultant array
+    
+    # TC: We are literally checking all permutations ie 2^N and for operation on string N
+    # hence TC: O(N* 2^N)
+    # SC: O(N): for recursion stack as its depth is maximum upto length of string
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        
-        
-# first of all we need to partition the string 
-# at each index and then check string upto that index is palindrome or not
-# if its palindrome then use recursion and check the partition for remaining string
-# with updated index 
-# to update index we need to loop through the string and each point repeat the process
-# ie check for palindrome and use recursion, and remember to remove that after recursion
-# as backtracking process.
-# now when the index goes out of string we will have the partitioned array
-# and append it to resultant array
         def ispali(s):
             return s==s[::-1]
         def solver(ind,s,temp,res):
