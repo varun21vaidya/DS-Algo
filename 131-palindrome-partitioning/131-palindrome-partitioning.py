8ddc48b94a8282030c571cce1dadc30e0f19a226
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-28 20:11:40 +0530

    Time: 1581 ms (17.90%), Space: 29 MB (88.36%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        
+# first of all we need to partition the string 
+# at each index and then check string upto that index is palindrome or not
+# if its palindrome then use recursion and check the partition for remaining string
+# with updated index 
+# to update index we need to loop through the string and each point repeat the process
+# ie check for palindrome and use recursion 
+# now when the index goes out of string we will have the partitioned array
+# and append it to resultant array
+        def ispali(s):
+            return s==s[::-1]
+        def solver(ind,s,temp,res):
+            if ind==len(s):
+                # print(temp)
+                res.append(temp[:])
+            
+            for i in range(ind,len(s)):
+                
+                if ispali(s[ind:i+1]):
+                    temp.append(s[ind:i+1])
+                    solver(i+1,s,temp,res)
+                    temp.pop()
+        temp,res=[],[]
+        solver(0,s,temp,res)
+        return res
