0066442186af1685350976603bd9fbd5c01aa3d9
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-09-29 23:55:29 +0530

    Time: 831 ms (13.44%), Space: 15.6 MB (27.45%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,5 +1,38 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        
+
+#         def binarysearch(arr, item):
+#             low, high = 0, len(arr)-1
+#             while low <= high:
+#                 mid = low+(high-low)//2
+#                 if arr[mid] == item:
+#                     return mid
+#                 elif item > arr[mid]:
+#                     low = mid+1
+#                 else:
+#                     high = mid-1
+#             return -1
+
+#         ind=binarysearch(arr, x)
+#         print(ind)
+#         if ind==-1:
+#             lst=[]
+#         else:
+#             lst = [x]
+#         i, j = 1, 1
+#         while len(lst) < k:
+#             print(lst)
+#             if (arr[ind+i]-arr[ind]) < (arr[ind]-arr[ind-j]):
+#                 lst.append(arr[ind+i])
+#                 if (ind+i) < len(arr):
+#                     i += 1
+#             else:
+#                 lst.append(arr[ind-j])
+#                 if (ind-j) > 0:
+#                     j += 1
+#         return sorted(lst)
+
         low,high=0,len(arr)-k
         while low<high:
             mid=low+(high-low)//2
@@ -9,4 +42,5 @@ class Solution:
                 high=mid
             
         return arr[low:low+k]
-                    
+            
+        
