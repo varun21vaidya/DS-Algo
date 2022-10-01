0b329ded91bc7a098cc23b1288e0ae2e57c71dd9
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-01 20:11:25 +0530

    Time: 65 ms (19.56%), Space: 14 MB (15.37%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -5,11 +5,9 @@ class Solution:
         heapq.heapify(queue)
         while len(queue)>1:
             a,b=(-heapq.heappop(queue)),(-heapq.heappop(queue))
-            print(a,b)
             if a-b!=0:
                 temp=a-b
                 heapq.heappush(queue,-temp)
-            print(queue)
         if queue: return -queue[0]
         else: return 0
                 
