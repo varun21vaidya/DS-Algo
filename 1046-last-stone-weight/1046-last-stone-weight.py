122e00737653aae357b47a9e5b2e619cdb578605
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-01 20:16:17 +0530

    Time: 54 ms (49.48%), Space: 13.9 MB (62.09%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,5 +1,27 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        
+#         # intuation approach
+#         if len(stones)==1:
+#             return stones[0]
+#         while len(stones)>1:
+#             stones.sort()
+#             if stones[-1]==stones[-2]:
+#                 stones.pop()
+#                 stones.pop()
+#             else:
+#                 temp=stones.pop()-stones.pop()
+#                 stones.append(temp)
+#             if len(stones)==0:
+#                 return 0
+#             if len(stones)==1:
+#                 return stones[0]
+            
+            
+        # Using Priority Queue 
+        # but as python uses min priority que values are made negative
+        
         import heapq
         queue=[-x for x in stones]
         heapq.heapify(queue)
