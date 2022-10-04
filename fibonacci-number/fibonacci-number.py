fb39ffd80183dc1eab715389e7b924da0973159b
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-04 12:33:27 +0530

    Time: 3427 ms, Memory: 13.9 MB - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n<2:
-            return n
-        else:
-            return self.fib(n-1) + self.fib (n-2)
+        # if n<2:
+        #     return n
+        # else:
+        #     return self.fib(n-1) + self.fib (n-2)
+        
+        def solver(n,mapp):
+            if n<2:
+                mapp[n]=mapp.get(n,n)
+            else:
+                mapp[n]=mapp.get(n,solver(n-1,mapp)+solver(n-2,mapp))
+            return mapp[n]
+        
+        mapp={}
+        return solver(n,mapp)
