d7d562764c2652313367efda55967d58b105cb02
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-07 12:19:58 +0530

    Time: 64 ms (14.96%), Space: 13.9 MB (62.26%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,5 +1,26 @@
 class Solution:
     def numTrees(self, n: int) -> int:
+
+    # # BEHIND THE INTUTION
+    
+    # We know that all node in left subtree are smaller than root and in right subtree are larger than root
+    # so if we have ith number as root, all numbers from 1 to i-1 will be in left subtree and
+    # i+1 to N will be in right subtree. 
+    # If 1 to i-1 can form x different trees and i+1 to N can from y different trees 
+    # then we will have x*y total trees when ith number is root and we also have N choices for root
+    # also so we can simply iterate from 1 to N for root and another loop for left and right subtree.
+    # If we take a closer look, we can notice that the count is basically n’th Catalan number.
+    
+    # Catalan numbers are defined as a mathematical sequence that consists of positive integers,
+    # which can be used to find the number of possibilities of various combinations. 
+    
+    # Approach: 
+    
+    # Base condition for the recursive approach, when n <= 1, return 1
+    # Iterate from i = 0 to i < n
+    # Make a recursive call catalan(i) and catalan(n – i – 1) and keep adding the product of both into res.
+    # Return the res.
+    
         def catlon(n):
             if n<=1:
                 return 1
