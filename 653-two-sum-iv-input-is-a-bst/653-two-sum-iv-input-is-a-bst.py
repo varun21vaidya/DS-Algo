18ce9f858cc202875f537a3f4ea9fd3cf2158d94
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-09 12:31:24 +0530

    Time: 178 ms (35.72%), Space: 18.2 MB (59.20%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -7,25 +7,38 @@
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
         
+#         # Inorder Traversal and Two Pointer
         
-        def solver(root,lst):
-            if not root: return
-            solver(root.left,lst)
-            lst.append(root.val)
-            solver(root.right,lst)  
+#         def solver(root,lst):
+#             if not root: return
+#             solver(root.left,lst)
+#             lst.append(root.val)
+#             solver(root.right,lst)  
         
-        lst=[]
-        solver(root,lst)
-        # print(lst)
+#         lst=[]
+#         solver(root,lst)
+#         # print(lst)
         
+#         left,right=0,len(lst)-1
+#         while left<right:
+#             summ=lst[left]+lst[right]
+#             if summ<k:
+#                 left+=1
+#             elif summ>k:
+#                 right-=1
+#             else:
+#                 return True
+#         return False
+
+
+
+        def dfs(root,sett):
+            if not root: return False
+            
+            diff= k-root.val
+            if diff in sett: return True
+            
+            sett.add(root.val)
+            return dfs(root.left,sett) or dfs(root.right,sett)
         
-        left,right=0,len(lst)-1
-        while left<right:
-            summ=lst[left]+lst[right]
-            if summ<k:
-                left+=1
-            elif summ>k:
-                right-=1
-            else:
-                return True
-        return False
+        return dfs(root, set())
