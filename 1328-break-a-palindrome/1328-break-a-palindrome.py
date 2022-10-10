3ae708676c2912db1cd89c4bae11d993bab71ec4
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-10 13:26:40 +0530

    Time: 56 ms (40.52%), Space: 13.8 MB (61.62%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,28 +1,55 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
+#         n=len(palindrome)
+        
+#         # if its only any single letter we cant replace it still and make it non-palindrome
+#         # so just return empty string
+        
+#         if n==1:
+#             return ""
+        
+#         # to get lexographilcally smallest non palindrome
+#         # we have to observe that it would be contain 'a' first
+#         # but if its already have 'a' initially then 
+#         # the next non-'a' would be replaced with 'a'
+        
+#         st=""
+#         for i in range(n):
+#             if palindrome[i]!='a':
+#                 st+='a'
+#                 st+=palindrome[i+1:]
+#                 if st!=st[::-1]:
+#                     return st
+#                 else: break
+#             st+=palindrome[i]
+            
+#         # but what if all are already 'a' 'aaaaaa'
+#         # we need to change last letter with 'b' while still making it smallest lexographically
+        
+#         return palindrome[:-1]+'b'
+
+
+
+        # instead of using extra space for 'st'
+        # what was corner case was that 'aba'
+        # when we changed b it became palindrome 'aaa'
+        # so with using extra space we had to check if it was palindrom or not
+        # but if we just check upto half of that string we could avoid that condition
+        # as it will conclude either that if we traverse upto half and no change is there 
+        # it means either whole string is 'aaaa' or middle character is non-'a'
+        # in both case we have to change then last character only
+        
+        # so we can ignore the condition with just taking loop upto n//2-1
+        # and remove that st and its palindrome check
+        
         n=len(palindrome)
         
-        # if its only any single letter we cant replace it still and make it non-palindrome
-        # so just return empty string
+        if n==1: return ''
         
-        if n==1:
-            return ""
-        
-        # to get lexographilcally smallest non palindrome
-        # we have to observe that it would be contain 'a' first
-        # but if its already have 'a' initially then the next non-'a' would be replaced with 'a'
-        
-        st=""
-        for i in range(n):
+        for i in range(n//2):
             if palindrome[i]!='a':
-                st+='a'
-                st+=palindrome[i+1:]
-                if st!=st[::-1]:
-                    return st
-                else: break
-            st+=palindrome[i]
-            
-        # but what if all are already 'a' 'aaaaaa'
-        # we need to change last letter with 'b' while still making it smallest lexographically
-        
+                return palindrome[:i]+'a'+palindrome[i+1:]
+           
+        # if conditions are 'aabaa' or 'aaaa' change last character with b
         return palindrome[:-1]+'b'
+            
