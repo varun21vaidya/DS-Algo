e1d0a79859f40b0c421c05bf152063bc5f21497d
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-11 16:04:46 +0530

    Time: 1494 ms (22.51%), Space: 24.5 MB (95.56%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -8,8 +8,21 @@ class Solution:
         #             if nums[i] < nums[j] < nums[k]:
         #                 return True
         # return False
+        
+        
+        
     
         # OPTIMAZTION FOR O(n) and constant space
+        
+#         we will traverse through nums array and give smallest elemeent as i
+#         if next element is large will get assigned to j and 
+#         if its small again assigned to i, as we need smaller element for i
+#         if both of them are full and further greater element occurse 
+#         it means we got solution, return True
+    
+#         if doesnt return after loops means its either same elements or decreasing 
+#         so return False
+        
         i, j = float('inf'), float('inf')
         for digit in range(len(nums)):
             if nums[digit]<=i:
