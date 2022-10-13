5ccc134ff5518d5b8ed7a5a87fef5755350fb198
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-13 10:41:58 +0530

    Time: 101 ms (17.21%), Space: 16.6 MB (45.66%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,22 +7,33 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-#       we know that inorder traversal for tree will always have sorted sequence
-#       so we can validate a tree based on this, if any tree doesnt have increasing sequence
-#       we will return False
-#       so first traverse inorder and save it in array
-#       and using linear scan check if it is increasing, if not return False, if loop ends return True
-        def inorder(root,output):
-            if root:
-                inorder(root.left,output)
-                output.append(root.val)
-                inorder(root.right,output)
+# #       we know that inorder traversal for tree will always have sorted sequence
+# #       so we can validate a tree based on this, if any tree doesnt have increasing sequence
+# #       we will return False
+# #       so first traverse inorder and save it in array
+# #       and using linear scan check if it is increasing, if not return False, if loop ends return True
+#         def inorder(root,output):
+#             if root:
+#                 inorder(root.left,output)
+#                 output.append(root.val)
+#                 inorder(root.right,output)
                 
-        res=[]
-        inorder(root,res)
-        for i in range(len(res)-1):
-            if res[i]>=res[i+1]:
-                return False
+#         res=[]
+#         inorder(root,res)
+#         for i in range(len(res)-1):
+#             if res[i]>=res[i+1]:
+#                 return False
             
-        return True
+#         return True
+    
+
+#     WITHOUT EXTRA SPACE 
+        def validate(root,minimum, maximum):
+            if not root: return True
+            if not minimum < root.val< maximum: return False
+            
+            return validate(root.left, minimum,root.val) and validate(root.right,root.val,maximum)
+        
+        return validate(root,float('-inf'),float('inf'))
+        
             
