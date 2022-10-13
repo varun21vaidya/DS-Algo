d6c4b79350818527d4e6f694dd215c43f2e7325d
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-13 12:32:16 +0530

    Time: 88 ms (41.67%), Space: 16.6 MB (45.66%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -28,6 +28,22 @@ class Solution:
     
 
 #     WITHOUT EXTRA SPACE 
+
+#     we will check if root.val>root.left.val and root.val<root.right.val
+#     so first we will set base condition that if root is at end ie None return True
+#     as it indicates full tree has been scanned and no False occurs
+#     now we create two variables for comparison with root.val, which are minimum and maximum
+#     we will scan the tree first by left side and then by right side both will validate
+#     their own left subtrees and right subtrees
+#     to validate on left we have to check root.val>root.left.val
+#     and as we have minimum < root.val< maximum,
+#     so as root moves downwards to left we will keep reassigning root to root.left
+#     we will have to keep minimum always, and compare with root.val and max
+#     so for each recursion root will be root.left and max will be root.val
+#     Similarly for right subtrees, root will be root.right so root.val should be less than that 
+#     so it will be assigned to minimum and maximum will stay max 
+#     finally for whole tree we will assign max ani min as float('inf') and float('-inf')
+
         def validate(root,minimum, maximum):
             if not root: return True
             if not minimum < root.val< maximum: return False
