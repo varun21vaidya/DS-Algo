b87f5ba3fc4cf7948c66b95e001da8c797fa5f6b
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-20 20:23:02 +0530

    Time: 77 ms (86.51%), Space: 43.8 MB (97.72%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,20 +3,41 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    const arr=[];
+    // const arr=[];
+    // for (let i=1; i<=n; i++){
+    //     if (i%15==0){
+    //         arr.push('FizzBuzz')
+    //     }
+    //     else if (i%3==0){
+    //         arr.push('Fizz')
+    //     }
+    //     else if (i%5==0){
+    //         arr.push('Buzz')
+    //     }
+    //     else{
+    //         arr.push(i.toString())
+    //     }
+    // }
+    // return arr
+    
+    s3=0
+    s5=0
+    s=""
+    const arr=[]
     for (let i=1; i<=n; i++){
-        if (i%15==0){
-            arr.push('FizzBuzz')
+        s3++
+        s5++
+        if (s3==3) {
+            s+="Fizz";
+            s3=0
         }
-        else if (i%3==0){
-            arr.push('Fizz')
+        if (s5==5) {
+            s+="Buzz";
+            s5=0
         }
-        else if (i%5==0){
-            arr.push('Buzz')
-        }
-        else{
-            arr.push(i.toString())
-        }
+        if (s.length===0){arr.push(i.toString())}
+        else{arr.push(s)}
+        s=""
     }
     return arr
 };
