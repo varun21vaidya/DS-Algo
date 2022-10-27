a392b2dd9134524694c4966bac7c613ca10f6319
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-27 23:03:11 +0530

    Time: 553 ms (86.55%), Space: 14.4 MB (64.33%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestOverlap(self, A, B):
+        N = len(A)
+        LA = [i // N * 100 + i % N for i in range(N * N) if A[i // N][i % N]]
+        LB = [i // N * 100 + i % N for i in range(N * N) if B[i // N][i % N]]
+        c = collections.Counter(i - j for i in LA for j in LB)
+        return max(c.values() or [0])
+        
