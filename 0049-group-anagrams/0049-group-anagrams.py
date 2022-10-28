3fd433400b48a4e3f990b72fd5cef8c022bc5308
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-29 00:42:40 +0530

    Time: 248 ms (32.05%), Space: 18.1 MB (50.70%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        words=defaultdict(list)
-        for char in strs:
-            words[tuple(sorted(char))].append(char)
-            # print(words)
-        return list(words.values())
+        mapp={}
+        for i in strs:
+            key=tuple(sorted(i))
+            if key in mapp:
+                mapp[key].append(i)
+            else:
+                mapp[key]=[i]
+        return mapp.values()
+        
