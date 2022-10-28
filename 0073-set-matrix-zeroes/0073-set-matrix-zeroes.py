52d1b068ba835ea96d45a8fdece4c16c9a5c9946
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-28 13:42:41 +0530

    Time: 350 ms (18.97%), Space: 14.9 MB (16.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,10 +3,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        
+        
+        # TC: O(m*n) SC: O(m+n)
+        
+#       traverse through matrix and if the ele is zero store its row and col in saperate arrays
+#       and traverse through that stored row array and make those rows zeros.
+#       traverse through all rows and thorough stored col array and make those cols zeros
+
+#       function to make entire row zero
+
         def setrows(setzerorow):
             for i in setzerorow:
                 matrix[i]=[0]*cols
-        
+                
+#       function to make entire column zero
+
         def setcols(setzerocol):
             for i in range(rows):
                 for j in setzerocol:
@@ -20,6 +32,7 @@ class Solution:
                 if matrix[i][j]==0:
                     setzerorow.append(i)
                     setzerocol.append(j)
+                    
         setrows(setzerorow)
         setcols(setzerocol)
         
