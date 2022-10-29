6b11bff4cd87c09b3279c897be8da6e3d202028f
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-29 22:26:42 +0530

    Time: 268 ms (100.00%), Space: 14 MB (100.00%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
+        
         res=[]
         for i in queries:
             for j in dictionary:
-                if len(i)==len(j):
-                    cnt=0
-                    for char in range(len(i)):
-                        if i[char]!=j[char]:
-                            cnt+=1
+                cnt=0
+                for char in range(len(i)):
+                    if i[char]!=j[char]:
+                        cnt+=1
                 if cnt<=2:
                     res.append(i)
                     break                        
