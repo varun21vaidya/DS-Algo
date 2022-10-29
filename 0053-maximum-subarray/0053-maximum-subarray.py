83167e6b3e1e6e243fb67ecdd00f426efecdbc10
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-29 19:16:40 +0530

    Time: 1712 ms (48.72%), Space: 28.5 MB (10.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -10,18 +10,18 @@ class Solution:
         # return maxx
     
 #       Dynamic Programming - KADANE's Algorithm
-        # curr=0
-        # maxsum=float('-inf')
-        # for i in nums:
-        #     curr+=i
-        #     if curr<0:
-        #         curr=0
-        #     elif curr>maxsum:
-        #         maxsum=curr
-        # return maxsum
+        curr=0
+        maxsum=float('-inf')
+        for i in nums:
+            curr+=i
+            if curr>maxsum:
+                maxsum=curr
+            if curr<0:
+                curr=0
+        return maxsum
         
-        currmax,maxx=0,nums[0]
-        for i in range(len(nums)):
-            currmax=max(nums[i], nums[i]+currmax)
-            maxx=max(maxx, currmax)
-        return maxx
+        # currmax,maxx=0,nums[0]
+        # for i in range(len(nums)):
+        #     currmax=max(nums[i], nums[i]+currmax)
+        #     maxx=max(maxx, currmax)
+        # return maxx
