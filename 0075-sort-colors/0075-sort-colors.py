75e478e8181d84013babb7c1a95c6be58a3a5457
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-30 16:32:32 +0530

    Time: 43 ms (82.53%), Space: 13.8 MB (63.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,22 +3,34 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # counting sort (2 passes)
-        red,white=0,0
-        for i in nums:
-            if i==0: red+=1
-            elif i==1: white+=1
+#         # counting sort (2 passes ie O(N)+O(N))
+#         red,white=0,0
+#         for i in nums:
+#             if i==0: red+=1
+#             elif i==1: white+=1
                 
-        for i in range(len(nums)):
-            if red>0:
-                red-=1
-                nums[i]=0
-            elif white>0:
-                white-=1
-                nums[i]=1
-            else:
-                nums[i]=2
+#         for i in range(len(nums)):
+#             if red>0:
+#                 red-=1
+#                 nums[i]=0
+#             elif white>0:
+#                 white-=1
+#                 nums[i]=1
+#             else:
+#                 nums[i]=2
                 
         
-                
-            
+        # Dutch National Flag Algorithm (1 pass O(N))
+        low,mid,high =0,0,len(nums)-1
+        
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                mid+=1
+                low+=1
+            elif nums[mid]==2:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+            else:
+                mid+=1
+        
