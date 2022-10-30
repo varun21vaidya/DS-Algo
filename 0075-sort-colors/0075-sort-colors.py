87e70e239d476f6d754fd4acdb46cb2c1339b948
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-30 16:00:50 +0530

    Time: 48 ms (77.65%), Space: 13.9 MB (15.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,8 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)-1):
-            for j in range(len(nums)-1):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-        
+        # counting sort
+        red=nums.count(0)
+        white= nums.count(1)
+        for i in range(len(nums)):
+            if red>0:
+                red-=1
+                nums[i]=0
+            elif white>0:
+                white-=1
+                nums[i]=1
+            else:
+                nums[i]=2
+                
+            
