e7827d1d0dba1c7948fbfdf229fe83a0ed516070
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-30 12:49:48 +0530

    Time: 3970 ms (16.67%), Space: 59.7 MB (100.00%) - LeetHub

---
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.py
@@ -0,0 +1,27 @@
+class Solution:
+    def mostPopularCreator(self, creators: List[str], ids: List[str], views: List[int]) -> List[List[str]]:
+        mostview={}
+        maxview={}
+        for i in range(len(creators)):
+            if creators[i] in mostview:
+                mostview[creators[i]]+=views[i]
+                if views[i]==maxview[creators[i]][1]:
+                    if maxview[creators[i]][0]>ids[i]:
+                        # print(maxview[creators[i]][0],ids[i])
+                        maxview[creators[i]][0]=ids[i]
+                if views[i]>maxview[creators[i]][1]:
+                    maxview[creators[i]]=[ids[i],views[i]]
+            else:
+                mostview[creators[i]]=views[i]
+                maxview[creators[i]]=[ids[i],views[i]]
+            
+        m=max(list(mostview.values()))
+        maxcreators=[i for i,j in mostview.items() if j==m]
+        # print(maxcreators)
+        # print(maxview)
+        res=[]
+        for i in maxcreators:
+            res.append([i, maxview[i][0]])
+        # print(res)
+        return res
+            
