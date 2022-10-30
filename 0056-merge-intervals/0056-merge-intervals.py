e293b001893cdf7af658ca7299830c3e12906a6d
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-10-31 01:05:06 +0530

    Time: 345 ms (39.22%), Space: 18.1 MB (53.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,8 +1,20 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        # if interval size is 1 directly return same intervals array
         if len(intervals)==1: return intervals
+        
+        # create a res array and sort the intervals according to start digit
         res=[]
         intervals=sorted(intervals,key= lambda x : x[0])
+        
+        # traverse through intervals and if there is no interval add in res
+        # also check if start of current interval is less than previous interval 
+        # if not then add current interval to res array
+        # if yes then this previous interval will be updated with current intervals
+        # but its start will be as it is as we had already sorted it
+        # so update the end value with max of current intervals end and last intervals end
+        # return the res array
         for i in range(len(intervals)):
             if not res or intervals[i][0]>res[-1][1]:
                 res.append(intervals[i])
