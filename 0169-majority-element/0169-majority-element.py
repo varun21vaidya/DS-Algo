10b9c80284616649ca93922bfe86403e677f302a
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-06 19:30:18 +0530

    Time: 178 ms (89.09%), Space: 15.4 MB (85.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # mapp=dict()
         n=len(nums)
+        
+        # mapp=dict()
         # for i in nums:
         #     if i not in mapp:
         #         mapp[i]=1
@@ -11,5 +12,17 @@ class Solution:
         #             break
         # return i
         
-        return sorted(nums)[n//2]
+        # return sorted(nums)[n//2]
+        
+        candidate =0
+        count= 0
+        
+        for value in nums:
+            if count==0:
+                candidate=value
+            if candidate==value:
+                count+=1
+            else: 
+                count-=1
+        return candidate
         
