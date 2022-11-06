dadb1ae40353b211d94a389061ce8f2b56119782
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-06 16:54:20 +0530

    Time: 85 ms (61.06%), Space: 14.3 MB (89.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -2,16 +2,31 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
         
-        def binary_search(nums, target):
-            l,r=0,len(nums)-1
-            while l<r:
-                mid=l+(r-l)//2
-                if nums[mid]<target:
-                    l=mid+1
-                else:
-                    r=mid
-            return True if target==nums[l] else False
+#         def binary_search(nums, target):
+#             l,r=0,len(nums)-1
+#             while l<r:
+#                 mid=l+(r-l)//2
+#                 if nums[mid]<target:
+#                     l=mid+1
+#                 else:
+#                     r=mid
+#             return True if target==nums[l] else False
         
-        x= [i for row in matrix for i in row]
-        # print(x)
-        return binary_search(x,target)
+# #       Brute Force O(n^2)- to convert 2d matrix to 1d matrix
+#         # x= [i for row in matrix for i in row]
+#         # # print(x)
+#         # return binary_search(x,target)
+    
+#         
+        m,n=len(matrix),len(matrix[0])
+        low,high=0,m*n-1
+        while low<=high:
+            mid=low+(high-low)//2
+            if matrix[mid//n][mid%n]==target:
+                return True
+            elif matrix[mid//n][mid%n]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        return False
+                
