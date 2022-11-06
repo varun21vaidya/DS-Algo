95ec7ae9d78aa32db078cb7761145090ad3996b8
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-06 17:44:59 +0530

    Time: 441 ms (24.60%), Space: 15.5 MB (85.94%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        mapp=dict()
+        # mapp=dict()
         n=len(nums)
-        for i in nums:
-            if i not in mapp:
-                mapp[i]=1
-            else:
-                mapp[i]+=1
-                if mapp[i]>n//2:
-                    break
-        return i
+        # for i in nums:
+        #     if i not in mapp:
+        #         mapp[i]=1
+        #     else:
+        #         mapp[i]+=1
+        #         if mapp[i]>n//2:
+        #             break
+        # return i
+        
+        return sorted(nums)[n//2]
         
