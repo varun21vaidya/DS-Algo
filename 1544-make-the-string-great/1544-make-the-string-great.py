74c237c71c1f4c25ec7e7e7a2fa60e5892ac99ef
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-09 00:23:36 +0530

    Time: 66 ms (58.17%), Space: 13.9 MB (15.06%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,12 +1,25 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        def check(s):
-            return ord(s[i])+32==ord(s[i+1]) or ord(s[i])==ord(s[i+1])+32
-        for _ in range(len(s)//2):
-            for i in range(len(s)-1):
-                if check(s):
-                    s=s.replace(s[i:i+2],"")
-                    break
-        return (s)
+        # Brute Force
+        
+        # def check(s):
+        #     return ord(s[i])+32==ord(s[i+1]) or ord(s[i])==ord(s[i+1])+32
+        # for _ in range(len(s)//2):
+        #     for i in range(len(s)-1):
+        #         if check(s):
+        #             s=s.replace(s[i:i+2],"")
+        #             break
+        # return (s)
 
+        stack=[]
+        for i in s:
+            if stack:
+                if abs(ord(stack[-1])-ord(i))==32:
+                    stack.pop()
+                else:
+                    stack.append(i)
+                
+            else:
+                stack.append(i)
+        return "".join(stack)
     
