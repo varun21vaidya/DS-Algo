6f479264a7a6b41a6ec90d3ba99b0bd6be0f2b36
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-09 00:27:14 +0530

    Time: 53 ms (76.90%), Space: 13.8 MB (97.60%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -2,6 +2,7 @@ class Solution:
     def makeGood(self, s: str) -> str:
         # Brute Force
         
+        # TC: O(N^2), SC: O(N^2)
         # def check(s):
         #     return ord(s[i])+32==ord(s[i+1]) or ord(s[i])==ord(s[i+1])+32
         # for _ in range(len(s)//2):
@@ -11,6 +12,17 @@ class Solution:
         #             break
         # return (s)
 
+        
+        
+        # Stack Approach
+        
+        # first loop through string
+        # if there is no element in stack, append current element
+        # if last element and current element makes uppercase & lowercase pair
+        # pop last element from stack and avoid current element
+        # else append current element to stack
+        
+        # TC: O(N), SC: O(N)
         stack=[]
         for i in s:
             if stack:
