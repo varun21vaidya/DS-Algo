554d8b92be1b3f12731cba97753b3735630f20d1
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-10 10:03:36 +0530

    Time: 1343 ms (5.02%), Space: 19.7 MB (14.93%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,23 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack=[]
+        self.res=[]
+    def next(self, price: int) -> int:
+        self.stack.append(price)
+        n=len(self.stack)
+        counter=1
+        if n>1:
+            if price>=self.stack[-2]:
+                counter+=self.res[-1]
+                for i in range(n-counter-1,-1,-1):
+                    if self.stack[i]<=price:
+                        counter+=1
+                    else:
+                        break
+        self.res.append(counter)
+        return counter
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
