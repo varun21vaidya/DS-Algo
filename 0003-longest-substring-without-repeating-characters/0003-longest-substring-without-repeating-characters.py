a6104043e479150fd56000ed26783bf0041347f7
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-21 19:47:15 +0530

    Time: 111 ms (72.96%), Space: 14 MB (93.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,17 +1,26 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        low,high,count,maxx=0,0,0,0
-        sett=set()
-        n=len(s)
-        while high<n:
-            if s[high] in sett:
-                maxx=max(maxx,high-low)
-                # print(s[high],maxx)
-                while s[high] in sett:
-                    sett.discard(s[low])
-                    low+=1
+#         low,high,count,maxx=0,0,0,0
+#         sett=set()
+#         n=len(s)
+#         while high<n:
+#             if s[high] in sett:
+#                 maxx=max(maxx,high-low)
+#                 # print(s[high],maxx)
+#                 while s[high] in sett:
+#                     sett.discard(s[low])
+#                     low+=1
 
-            sett.add(s[high])
+#             sett.add(s[high])
 
-            high+=1
-        return max(maxx,high-low)
+#             high+=1
+#         return max(maxx,high-low)
+
+        mapp={}
+        l,maxx=0,0
+        for r in range(len(s)):
+            if s[r] in mapp:
+                l=max(l,mapp[s[r]]+1)
+            mapp[s[r]]=r
+            maxx=max(maxx, r-l+1)
+        return maxx
