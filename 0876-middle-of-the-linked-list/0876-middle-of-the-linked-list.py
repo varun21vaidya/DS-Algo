80f8c00de51ce4cad8a77aecbad004bf1cf9e49d
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-22 15:46:51 +0530

    Time: 63 ms (25.85%), Space: 13.8 MB (96.23%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -10,17 +10,18 @@ class Solution:
 #       first traverse whole linked list
 #       calculate the length of list
 #       and traverse upto half of that length and return 
-        count=0
-        temp=head
-        while temp:
-            temp=temp.next
-            count+=1
-        cnt=count//2
-        while cnt>0:
-            head=head.next
-            cnt-=1
+
+#         count=0
+#         temp=head
+#         while temp:
+#             temp=temp.next
+#             count+=1
+#         cnt=count//2
+#         while cnt>0:
+#             head=head.next
+#             cnt-=1
         
-        return head
+#         return head
         
         
         # 2 pointer approach - 1 Pass Solution: O(n)
@@ -34,8 +35,12 @@ class Solution:
         # and after while loop return pointer1
     
         ptr1,ptr2=head,head
-        while ptr2.next or ptr2.next.next:
+        while ptr2.next:
+            if not ptr2.next.next:
+                ptr1=ptr1.next
+                break
             ptr1=ptr1.next
             ptr2=ptr2.next.next
+            
         return ptr1
         
