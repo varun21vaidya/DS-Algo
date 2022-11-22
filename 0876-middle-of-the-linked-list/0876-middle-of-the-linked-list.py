466eb22caca8935e574f64b7d2530cd5ccc221f2
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-22 15:43:59 +0530

    Time: 41 ms (79.55%), Space: 13.9 MB (56.29%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -5,6 +5,8 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # O(2N) - 2 pass solution
 #       first traverse whole linked list
 #       calculate the length of list
 #       and traverse upto half of that length and return 
@@ -20,6 +22,17 @@ class Solution:
         
         return head
         
+        
+        # 2 pointer approach - 1 Pass Solution: O(n)
+        # one pointer traverse with 1 step at a time
+        # while second pointer moves with 2 steps at time
+        # so pointer2 will be double faster and
+        # cover double distance than pointer1
+        # so when pointer1 is at mid, pointer 2 will be at end
+        # so check until pointer2.next for odd length and pointer2.next.next for even 
+        # and traverse both pointers 
+        # and after while loop return pointer1
+    
         ptr1,ptr2=head,head
         while ptr2.next or ptr2.next.next:
             ptr1=ptr1.next
