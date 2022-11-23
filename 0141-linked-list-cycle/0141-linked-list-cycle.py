829675accd29db23f868219c21b63a151a763066
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-23 23:52:40 +0530

    Time: 132 ms (21.43%), Space: 17.6 MB (68.12%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,7 +6,8 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-#         # method 1: sett
+        
+#         # method 1: sett TC: O(n) SC: O(n)
 #         sett=set()
         
 #         # run until head becomes null
@@ -24,6 +25,15 @@ class Solution:
 #         # it does not contain a cycle
 #         return False
     
+        # Method 2: Floyd's Algo TC: O(n) SC: O(1)
+        
+        # Now we can also do this, in constant time
+        # with Floyd’s Cycle-Finding Algorithm
+        # with using two pointers one uses 1 step at a time
+        # other moves 2 steps at a time
+        # if they both meet at any point, its a cycle
+        # else p2 will result null and return False
+        
         p1, p2 = head, head
         while p1 and p2 and p2.next:
             p1=p1.next
