e60b610e59dc922ab86c163248244c71145b7608
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-23 21:32:39 +0530

    Time: 349 ms (52.52%), Space: 29.6 MB (49.27%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -8,6 +8,7 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         
         # Brute Force-2 loops (TLE) 
+        # O(n^2)
         
         # temp=headB
         # while headA:
@@ -20,15 +21,61 @@ class Solution:
         #     headA=headA.next
         # return None
         
-        sett=set()
+        # Method 2 - Hashset
+        
+        # Use hashset and store list1 nodes in sett
+        # and again use traverse in list2 and lookup if 
+        # node is present in sett, if its there return it
+        # else return None
+        
+        # TC: O(n+m) SC: O(n)
+    
+#         sett=set()
+#         while headA:
+#             sett.add(headA)
+#             headA=headA.next
+#         # print(sett)
+#         while headB:
+#             if headB in sett:
+#                 return headB
+#             headB=headB.next
+        
+#         return None
+    
+        c1,c2=0,0
+        temp1,temp2=headA,headB
+        
+        # get count of both nodes
         while headA:
-            sett.add(headA)
+            c1+=1
             headA=headA.next
-        # print(sett)
+        
         while headB:
-            if headB in sett:
-                return headB
+            c2+=1
             headB=headB.next
         
+        # print(c1,c2)
+        
+        # if count1>count 2 traverse temp1 to c1-c2
+        # else traverse temp2 c2-c1
+        if c1>c2:
+            # print("list1 is longer")
+            while c1>c2:
+                temp1=temp1.next
+                c1-=1
+        else:
+            # print("list2 is longer")
+            while c2>c1:
+                temp2=temp2.next
+                c2-=1
+                
+        while temp1 and temp2:
+            if temp1==temp2:
+                # print("got the node")
+                return temp1
+            temp1=temp1.next
+            temp2=temp2.next
+        
         return None
+        
             
