20e6728e7c854aae355117732cfb121450d93e2b
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-23 23:49:21 +0530

    Time: 105 ms (63.61%), Space: 17.5 MB (95.10%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,20 +6,30 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        # method 1: sett
-        sett=set()
+#         # method 1: sett
+#         sett=set()
         
-        # run until head becomes null
-        while head:
+#         # run until head becomes null
+#         while head:
             
-            # if node is in sett, its a cycle, return True
-            if head in sett:
+#             # if node is in sett, its a cycle, return True
+#             if head in sett:
+#                 return True
+            
+#             # else add the node to sett and traverse list
+#             sett.add(head)
+#             head=head.next
+        
+#         # if head becomes null gets out of while loop
+#         # it does not contain a cycle
+#         return False
+    
+        p1, p2 = head, head
+        while p1 and p2 and p2.next:
+            p1=p1.next
+            p2=p2.next.next
+            if p1==p2:
                 return True
-            
-            # else add the node to sett and traverse list
-            sett.add(head)
-            head=head.next
         
-        # if head becomes null gets out of while loop
-        # it does not contain a cycle
         return False
+        
