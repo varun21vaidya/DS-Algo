510fe6ede6ec512f173d90d33724e26b7aaa8d35
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-24 23:17:46 +0530

    Time: 710 ms (98.86%), Space: 38.9 MB (90.52%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -6,18 +6,45 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        #method2: traverse the list, at the same time, append the nodes in the stack, now when reached Null, traverse from start, start poping from the stack, and again  compare both nodes, if stack gets empty and traverse reach Null return True, else Return False.
+        # method1: traverse the list, at the same time, append the nodes in the stack, now when reached Null, traverse from start, start poping from the stack, and again  compare both nodes, if stack gets empty and traverse reach Null return True, else Return False.
+
+        # temp=head
+        # stack=[]
+        # while temp:
+        #     stack.append(temp.val)            
+        #     temp=temp.next
+        # # print(stack)
+        # while head:
+        #     if head.val!=stack.pop():
+        #         return False
+        #     head=head.next
+        # return True
         
-        #method1: counta length of list, if even, reverse upto middle, compare both front side, and from middle to end
+        # method 2: use floyds algo and reach the middle of linked list
+        # then run another pointer from the start
         
-        temp=head
-        stack=[]
-        while temp:
-            stack.append(temp.val)            
-            temp=temp.next
-        # print(stack)
-        while head:
-            if head.val!=stack.pop():
+        #method2: first get to middle then reverse list
+        
+        slow,fast=head, head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        if fast: slow=slow.next
+            
+        prev=None
+        front=head
+        
+        while slow:
+            nex=slow.next
+            slow.next=prev
+            prev=slow
+            slow=nex
+            
+        while prev:
+            if front.val!=prev.val:
                 return False
-            head=head.next
+            front=front.next
+            prev=prev.next
         return True
+            
+            
