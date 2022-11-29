effcab6db991ccb175f1d1ed271044e7d51285c0
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-11-29 16:28:42 +0530

    Time: 36 ms (94.66%), Space: 15 MB (46.28%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -10,40 +10,40 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         
-        # Method 1 : hashmap TC: O(n), SC:O(n)
-        # create a linkedlist similar to input given
-        # but why, because we cant get input list directly
-        # so store its nodes in hashmap
+#         # Method 1 : hashmap TC: O(n), SC:O(n)
+#         # create a linkedlist similar to input given
+#         # but why, because we cant get input list directly
+#         # so store its nodes in hashmap
         
         
-        # clone the list without random
-        # use a hashmap which will link og list with clone list
-        # now traverse both list at same time and get random pointer.
-        # first get its random node from og list then with key as value to it get its clone value from hashmap
-        # and assign clone.random to that value
-        # more clear explanation: https://leetcode.com/problems/copy-list-with-random-pointer/solutions/258935/detailed-explanation-with-pictures-c-javascript/
+#         # clone the list without random
+#         # use a hashmap which will link og list with clone list
+#         # now traverse both list at same time and get random pointer.
+#         # first get its random node from og list then with key as value to it get its clone value from hashmap
+#         # and assign clone.random to that value
+#         # more clear explanation: https://leetcode.com/problems/copy-list-with-random-pointer/solutions/258935/detailed-explanation-with-pictures-c-javascript/
 
-        mapp={}
-        op=res=Node(0)
-        temp=head
+#         mapp={}
+#         op=res=Node(0)
+#         temp=head
 
-        # first pass to create a clone and allocate clone nodes as values
-        # to its og list nodes as keys in a hashmap
-        while temp:
-            res.next=Node(temp.val)
-            mapp[temp]=res.next
-            temp=temp.next
-            res=res.next
+#         # first pass to create a clone and allocate clone nodes as values
+#         # to its og list nodes as keys in a hashmap
+#         while temp:
+#             res.next=Node(temp.val)
+#             mapp[temp]=res.next
+#             temp=temp.next
+#             res=res.next
 
-        temp=head   # og list
-        res=op.next # clone list
+#         temp=head   # og list
+#         res=op.next # clone list
 
-        # 2nd pass to allocate the random pointers to clone list
-        while temp:
-            res.random= mapp[temp.random] if temp.random else None
-            temp,res=temp.next, res.next
+#         # 2nd pass to allocate the random pointers to clone list
+#         while temp:
+#             res.random= mapp[temp.random] if temp.random else None
+#             temp,res=temp.next, res.next
         
-        return op.next
+#         return op.next
 
 
 
