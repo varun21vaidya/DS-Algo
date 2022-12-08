110671933e737d565c34aa9c0ca1eecee3207319
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-12-08 15:58:48 +0530

    Time: 39 ms (86.76%), Space: 14.1 MB (83.26%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,31 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        # TC: n*2^n
+            
         n=len(nums)
+#         res=[]
+#         for i in range(1<<n):
+#             temp=[]
+            
+#             for j in range(n):
+#                 if i & 1<<j:
+#                     temp.append(nums[j])
+#             res.append(temp)
+            
+#         return res
+
+        # Recursion
+        n=len(nums) 
+        def solver(arr,ind,temp):
+            if ind>=n:
+                res.append(temp.copy())
+                return
+            temp.append(arr[ind])
+            solver(arr,ind+1,temp)
+            temp.pop()
+            solver(arr,ind+1, temp)
+            
         res=[]
-        for i in range(1<<n):
-            temp=[]
-            
-            for j in range(n):
-                if i & 1<<j:
-                    temp.append(nums[j])
-            res.append(temp)
-            
+        solver(nums,0,[])
         return res
