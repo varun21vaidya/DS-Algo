67f04051ec30420e3fb6ca552671b76a02d1615b
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-12-27 06:33:05 +0530

    Time: 59 ms (88.65%), Space: 15.4 MB (20.44%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,19 +1,34 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
+        
         def solver(s, temp, ind):
             
-            if ind < len(s):
-                if s[ind].isnumeric():
-                    temp += s[ind]
-                    ind += 1
+#             if ind < len(s):
+#                 if s[ind].isnumeric():
+#                     temp += s[ind]
+#                     ind += 1
 
-            if ind >= len(s):
-                ans.add(temp)
+#             if ind >= len(s):
+#                 ans.add(temp)
+#                 return
+
+#             solver(s, temp+s[ind], ind+1)
+#             solver(s, temp+(s[ind].swapcase()), ind+1)
+
+#         ans = set()
+#         solver(s, "", 0)
+#         return ans
+
+            if ind>=len(s):
+                ans.append(temp)
                 return
 
-            solver(s, temp+s[ind], ind+1)
-            solver(s, temp+(s[ind].swapcase()), ind+1)
+            if s[ind].isnumeric():
+                solver(s, temp+s[ind], ind+1)
+            else:
+                solver(s, temp+s[ind], ind+1)
+                solver(s, temp+(s[ind].swapcase()), ind+1)
 
-        ans = set()
+        ans=[]
         solver(s, "", 0)
         return ans
