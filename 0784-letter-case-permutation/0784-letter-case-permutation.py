2f5cf9c8dc2208e865fc26bfd1dc0dd603b1f30a
Varun <65745841+varun21vaidya@users.noreply.github.com>
2022-12-27 06:34:58 +0530

    Time: 50 ms (97.78%), Space: 15.4 MB (20.44%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -3,22 +3,6 @@ class Solution:
         
         def solver(s, temp, ind):
             
-#             if ind < len(s):
-#                 if s[ind].isnumeric():
-#                     temp += s[ind]
-#                     ind += 1
-
-#             if ind >= len(s):
-#                 ans.add(temp)
-#                 return
-
-#             solver(s, temp+s[ind], ind+1)
-#             solver(s, temp+(s[ind].swapcase()), ind+1)
-
-#         ans = set()
-#         solver(s, "", 0)
-#         return ans
-
             if ind>=len(s):
                 ans.append(temp)
                 return
