45e5375f56e06587049ffa00ae082dfe41743642
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-04 07:06:25 +0530

    Time: 3459 ms (5.09%), Space: 28 MB (99.18%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -10,12 +10,21 @@ class Solution:
         
         ans=0
         for freq in mapp.keys():
+            
+            # if any element has freq 1, immedietly return -1
             if mapp[freq]==1:
                 return -1
+            
+            # if freq can directly divided by 3
             elif mapp[freq]%3==0:
                 ans+=mapp[freq]//3
+                
             else:
-                # after using 3s there many be even number remaining whih will be handled by a 2 or there may be 1 remaining which will be handled by reducing one 3 and addding 2 
+                # if the number is not directly divided by 3
+                # first use as many 3s you can use
+                # after using 3s there many be 2 remaining which will be handled by a 2 or
+                # there may be 1 remaining which will be handled by reducing one 3 and addding 2 
                 # but any of them requires same count so extra one is used for handling with 2.
                 ans+=mapp[freq]//3+1 
+                
         return ans
