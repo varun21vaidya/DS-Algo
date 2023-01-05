e5d9b0d20aa6288e2cdff8ab9ead4b808e337a1c
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-05 06:53:53 +0530

    Time: 1788 ms (55.40%), Space: 59.9 MB (70.29%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,8 +1,15 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        
+        # Explanation:
+        # so we will sort the array according to max ending of each ballon
+        # ie end point ie 1st index
+        # now we will assign maxreach varaible to traverse and 
+        # check if any ballon goes out of its reach to shoot arrow
+        # if yes increament arrow counter, and update maxreach to current ballons end 
         if not points: return 0
         points.sort(key = lambda k : k[1] )
-        print(points)
+        # print(points)
         maxreach,arrow= points[0][1],1
         for i in range(1,len(points)):
             if points[i][0]>maxreach:
