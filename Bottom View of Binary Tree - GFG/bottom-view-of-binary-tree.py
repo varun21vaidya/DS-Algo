cec22510d99f7eb5869abad5fe6e1f501f24b5ce
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-09 17:50:03 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.py
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.py
@@ -2,49 +2,49 @@
 
 class Solution:
     def bottomView(self, root):
-        # import collections
-        # mapp=collections.defaultdict(list)
-        # q=collections.deque([[root,0,0]])
-        # while q:
-        #     for i in range(len(q)):
-        #         temp,col,level=q.popleft()
+        import collections
+        mapp=collections.defaultdict(list)
+        q=collections.deque([[root,0,0]])
+        while q:
+            for i in range(len(q)):
+                temp,col,level=q.popleft()
     
-        #         if temp.left:
-        #             q.append([temp.left,col-1,level+1])
-        #         if temp.right:
-        #             q.append([temp.right,col+1,level+1])
+                if temp.left:
+                    q.append([temp.left,col-1,level+1])
+                if temp.right:
+                    q.append([temp.right,col+1,level+1])
                 
-        #         if col in mapp:
-        #             if level>mapp[col][0]:
-        #                 mapp[col]=[level,temp.data]
-        #         else:
-        #             mapp[col]=[level,temp.data]
+                if col in mapp:
+                    if level>=mapp[col][0]:
+                        mapp[col]=[level,temp.data]
+                else:
+                    mapp[col]=[level,temp.data]
     
-        # # print(mapp)
-    
-        # return [mapp[x][1] for x in sorted(mapp)]
-
-
-    
-        def topview(root,col,level):
-            if not root: return
-    
-            if col in mapp:
-                if level>=mapp[col][0]:
-                    mapp[col]=[level,root.data]
-            else:
-                mapp[col]=[level,root.data]
-    
-            topview(root.left,col-1,level+1)
-            topview(root.right,col+1,level+1)
-    
-    
-        from collections import defaultdict
-        mapp=defaultdict(list)
-        topview(root,0,0)
         # print(mapp)
     
         return [mapp[x][1] for x in sorted(mapp)]
+
+
+    
+        # def topview(root,col,level):
+        #     if not root: return
+    
+        #     if col in mapp:
+        #         if level>=mapp[col][0]:
+        #             mapp[col]=[level,root.data]
+        #     else:
+        #         mapp[col]=[level,root.data]
+    
+        #     topview(root.left,col-1,level+1)
+        #     topview(root.right,col+1,level+1)
+    
+    
+        # from collections import defaultdict
+        # mapp=defaultdict(list)
+        # topview(root,0,0)
+        # # print(mapp)
+    
+        # return [mapp[x][1] for x in sorted(mapp)]
 
 #{ 
  # Driver Code Starts
