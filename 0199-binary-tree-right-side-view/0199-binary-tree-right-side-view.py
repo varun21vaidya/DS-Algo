10b7a44727624cde6043ac0ef23e58d8326700a0
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-09 20:27:10 +0530

    Time: 32 ms (89.66%), Space: 13.8 MB (69.20%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        
+#         def dfs(root,col,level):
+#             if not root: return 
+            
+#             if level in mapp:
+#                 print(col,level,root.val,)
+
+#                 if col>=mapp[level][0]:
+#                     mapp[level]=[col,root.val]
+#             else:
+#                 mapp[level]=[col,root.val]
+                
+#             dfs(root.left,col-1,level+1)
+#             dfs(root.right, col+1,level+1)
+        
+#         mapp=defaultdict(list)
+#         dfs(root,0,0)
+#         print(mapp)
+        
+#         return [mapp[x][1] for x in mapp]
+
+
+        if not root: return
+        q=deque([root])
+        ans=[]
+        while q:
+            for i in range(len(q)):
+                temp=q.popleft()
+                if temp.left:
+                    q.append(temp.left)
+                if temp.right:
+                    q.append(temp.right)
+                
+            ans.append(temp.val)
+        return ans
+            
