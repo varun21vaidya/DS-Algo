c195d9dfc910a3c1dd9f0c3d1d5feccca9f56e6e
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-09 11:12:51 +0530

    Time: 37 ms (69.88%), Space: 13.9 MB (57.68%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -6,16 +6,38 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        def helper(root):
-            if root:
-                helper(root.left)
-                res.append(root.val)
-                helper(root.right)
-        res=[]
-        helper(root)
-        return res
+#         def helper(root):
+#             if root:
+#                 helper(root.left)
+#                 res.append(root.val)
+#                 helper(root.right)
+#         res=[]
+#         helper(root)
+#         return res
     
-
+        # while there is current node, go to extreme left
+        # and append to stack each node
+        # now you will reach null ie curr becomes null
+        # now else, get element from top of stack, addd it to result and move to right
+        # now again it checks if something is to right
+        # at some point stack will become empty, break the loop
+        
+        curr=root
+        stack=[]
+        res=[]
+        while True:
+            if curr:
+                stack.append(curr)
+                curr=curr.left
+            else:
+                if not stack: return res
+                curr=stack.pop()
+                res.append(curr.val)
+                curr=curr.right
+                
+        return res
+                
+                
         
 
         
