c32013a5f687d309ca0bae516d5b14fc8298d624
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-16 23:11:24 +0530

    Time: 87 ms (72.71%), Space: 17.4 MB (52.22%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,20 +1,29 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        # corner case, of empty intervals
         if not intervals: return [newInterval]
-        n=len(intervals)
-        for i in range(n):
+        
+        insertion=False
+        
+        # add the insertion at sorted position
+        for i in range(len(intervals)):
             if intervals[i][0]>newInterval[0]:
                 intervals.insert(i,newInterval)
+                insertion=True
                 break
-        if len(intervals)==n: intervals.append(newInterval)
-        # print(intervals)
+          
+        # if newinterval not added,add it to last
+        if not insertion: intervals.append(newInterval)
+            
         
+        # if last value of intervals added is greater than first value of next ele
+        # update last value of intervals, else just append the new interval.
         new=[]
         for i in range(len(intervals)):
             if new and intervals[i][0]<=new[-1][1]:
                 new[-1][1]=max(intervals[i][1], new[-1][1])
             else:
                 new.append(intervals[i])
-        #     print(i,new)
-        # print("------------------------------------")
+
         return new
