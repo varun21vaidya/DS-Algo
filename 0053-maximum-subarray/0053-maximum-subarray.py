5f6d9181cf909c198c1466d9fc7d6edb2c867ca9
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-19 00:03:35 +0530

    Time: 727 ms (89.37%), Space: 28.6 MB (33.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,27 +1,50 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        
-#       brute force : (got TLE)
-        # curr, maxx=0,float('-inf')
-        # for i in range(len(nums)):
-        #     for j in range(i,len(nums)):
-        #         maxx=max(maxx, sum(nums[i:j+1]))
-        # return maxx
+     
+        totalsum,maxx=0,float('-inf')
+        for i in range(len(nums)):
+            totalsum+=nums[i]
+            maxx=max(maxx,totalsum)
+
+            if totalsum<0: totalsum=0
+            
+            
+        return maxx
     
-#       Dynamic Programming - KADANE's Algorithm
-        curr=0
-        maxsum=float('-inf')
-        for i in nums:
-            curr+=i
-            if curr>maxsum:
-                maxsum=curr
-            if curr<0:
-                curr=0
-        return maxsum
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         
-        # currmax,maxx=0,nums[0]
-        # for i in range(len(nums)):
-        #     currmax=max(nums[i], nums[i]+currmax)
-        #     maxx=max(maxx, currmax)
-        # return maxx
+# #       brute force : (got TLE)
+#         curr, maxx=0,float('-inf')
+#         for i in range(len(nums)):
+#             for j in range(i,len(nums)):
+#                 curr=sum(nums[i:j+1])
+#                 maxx=max(maxx, curr)
+#         return maxx
+    
+# #       Dynamic Programming - KADANE's Algorithm
+#         curr=0
+#         maxsum=float('-inf')
+#         for i in nums:
+#             curr+=i
+#             if curr>maxsum:
+#                 maxsum=curr
+#             if curr<0:
+#                 curr=0
+#         return maxsum
+        
+#         currmax,maxx=0,nums[0]
+#         for i in range(len(nums)):
+#             currmax=max(nums[i], nums[i]+currmax)
+#             maxx=max(maxx, currmax)
+#         return maxx
