134037a71351c83718bbe8bf3890f5b0ab3ef1e5
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-01-27 09:06:40 +0530

    Time: 628 ms (50.32%), Space: 32.6 MB (15.25%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -1,16 +1,41 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         
+#         hashset= set()
+#         for w in words: hashset.add(w)
+            
+#         def concatwords(word):
+            
+#             for i in range(1, len(word)):
+#                 prefix=word[:i]
+#                 suffix=word[i:]
+                
+#                 if prefix in hashset and (suffix in hashset or concatwords(suffix)):
+#                     return True
+        
+#         return [w for w in words if concatwords(w)]
+
+        # # Improve using Memoization:
+    
+        mem={}
         hashset= set()
         for w in words: hashset.add(w)
             
         def concatwords(word):
+            if len(word)==1: return False
+            if word in mem:
+                return mem[word]
+            
+            mem[word]=False
             
             for i in range(1, len(word)):
                 prefix=word[:i]
                 suffix=word[i:]
                 
                 if prefix in hashset and (suffix in hashset or concatwords(suffix)):
-                    return True
+                    mem[word]=True
+                    break
+                    
+            return mem[word]
         
         return [w for w in words if concatwords(w)]
