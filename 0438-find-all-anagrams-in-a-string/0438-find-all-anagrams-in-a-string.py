3019be1a819332a505cb5987db7ec50df2328dc7
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-02-05 17:49:14 +0530

    Time: 137 ms (67.54%), Space: 15.1 MB (74.41%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,10 +1,38 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        countp=Counter(p)
-        res=[]
-        for i in range(len(s)-len(p)+1):
-            new=Counter(s[i:i+len(p)])
-            if new==countp:
-                res.append(i)
-        return res
+        
+        # # Brute Force - HASHMAP
+        # countp=Counter(p)
+        # res=[]
+        # for i in range(len(s)-len(p)+1):
+        #     new=Counter(s[i:i+len(p)])
+        #     if new==countp:
+        #         res.append(i)
+        # return res
             
+        # sliding window
+        i,j,n=0,0, len(s)
+        k=len(p)
+        res=[]
+        countp=Counter(p)
+        temp={}
+        while j<n:
+            temp[s[j]]=temp.get(s[j],0)+1
+            
+            if j-i+1==k:
+                if temp==countp:
+                    res.append(i)
+                temp[s[i]]-=1
+                if temp[s[i]]==0:
+                    del temp[s[i]]
+                i+=1
+                j+=1
+            else:
+                j+=1
+                
+        return res
+                
+            
+            
+            
+        
