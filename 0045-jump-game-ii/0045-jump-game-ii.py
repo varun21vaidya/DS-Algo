3cb6f22b1a8eef687c8730e8bcff0778a70edbed
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-02-09 00:02:42 +0530

    Time: 125 ms (92.20%), Space: 15.1 MB (54.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,64 +1,35 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        mapp={}
-        @lru_cache()
-        def helper(index=0):
-            ans=float('inf')
-            if index>=len(nums)-1:
-                return 0
-            if index in mapp:
-                return mapp[index]
-            for i in range(index, index+nums[index]):
-                ans=min(ans,1+helper(i+1))
-            mapp[index]=ans    
-            return ans
+#         mapp={}
+#         @lru_cache()
+#         def helper(index=0):
+#             ans=float('inf')
+#             if index>=len(nums)-1:
+#                 return 0
+#             if index in mapp:
+#                 return mapp[index]
+#             for i in range(index, index+nums[index]):
+#                 ans=min(ans,1+helper(i+1))
+#             mapp[index]=ans    
+#             return ans
         
-        index=0
-        return helper(index)
+#         index=0
+#         return helper(index)
     
-        # # greedy
-        # i,n=0, len(nums)-1
-        # steps=0
-        # while i<=n:
-        #     if i==n:
-        #         return steps
-        #     steps+=1
-        #     maxx=-1
-        #     ind=0
-        #     for j in range(1,nums[i]+1):
-        #         if i+j==n:
-        #             return steps
-        #         if maxx<nums[i+j]:
-        #             maxx=nums[i+j]
-        #             ind=j
-        #     i=i+ind
+        # Greedy
+        l,r,steps=0,0,0
+        while r<len(nums)-1:
+            maxxdist=0
+            for i in range(l, r+1):
+                maxxdist=max(maxxdist, i+nums[i])
+            
+            l=r+1
+            r=maxxdist
+            steps+=1
         
-
-
-
-
-
-        # recursive
-        # n=len(nums)
-        # @lru_cache()
-        # def solver(curr,steps,minsteps):
-        #     if curr>=n:
-        #         return minsteps
+        return steps
             
-        #     if curr==n-1:
-        #         minsteps=min(steps,minsteps)
-        #         return minsteps
-            
-        #     for i in range(1, nums[curr]+1):
-        #         minsteps=solver(curr+i, steps+1,minsteps)
-
-        #     return minsteps
-    
-    
-        # return solver(0,0,n+1)
-
-
-
+        
 
 #         n = len(nums)
 #         jumps = [n] * n
