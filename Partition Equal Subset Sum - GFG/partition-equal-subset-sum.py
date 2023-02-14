277ba5f68d8b9bc94d6d6b3f61b13d6aa434b3b1
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-02-14 20:46:41 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.py
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.py
@@ -4,29 +4,40 @@ class Solution:
     def equalPartition(self, N, arr):
         # code here
         
-        # def solver(n,w):
-            # if n==0:
-            #     return 0
-            # if w==0:
-            #     return 1
+        def solver(n,w):
+            if n==0:
+                return 0
+            if w==0:
+                return 1
             
-            # if arr[n-1]<=w:
-            #     return ma
+            if arr[n-1]<=w:
+                return solver(n-1, w-arr[n-1]) or solver(n-1, w)
+            else:
+                return solver(n-1, w)
+        
         w=sum(arr)
         if w%2==1:
             return False
-        w=w//2
-        dp= [[0 for _ in range(w+1)] for _ in range(N+1)]
-        for firstcol in range(N+1):
-            dp[firstcol][0]=1
+        else:
+            w=w//2
+            return solver(N,w)
+            
+            
+        # w=sum(arr)
+        # if w%2==1:
+        #     return False
+        # w=w//2
+        # dp= [[0 for _ in range(w+1)] for _ in range(N+1)]
+        # for firstcol in range(N+1):
+        #     dp[firstcol][0]=1
         
-        for row in range(1,N+1):
-            for col in range(1,w+1):
-                if arr[row-1]<=col:
-                    dp[row][col]=(dp[row-1][col- arr[row-1]]  or dp[row-1][col])
-                else:
-                    dp[row][col]=dp[row-1][col]
-        return dp[N][w]
+        # for row in range(1,N+1):
+        #     for col in range(1,w+1):
+        #         if arr[row-1]<=col:
+        #             dp[row][col]=(dp[row-1][col- arr[row-1]]  or dp[row-1][col])
+        #         else:
+        #             dp[row][col]=dp[row-1][col]
+        # return dp[N][w]
 
 #{ 
  # Driver Code Starts
