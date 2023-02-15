571aee647c8c3c02f524306153da6a5fdd5c408a
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-02-16 01:23:56 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.py
+++ Rod Cutting - GFG/rod-cutting.py
@@ -29,14 +29,18 @@ class Solution:
 
 
         # # Bottom UP Approach:
-        length=[i for i in range(1,N+1)]
+        
+        # length=[i for i in range(1,N+1)] --> 1 2 3 4 5 6 7 8
+        # but we can directly eliminate by taking i directly instead of length[i-1]
+        # as we are anyway iterating i loop for that so no need for 2nd array
+        
         dp=[[0 for _ in range(N+1)] for _ in range(N+1)]
         for i in range(N+1):
             for j in range(N+1):
                 if i==0 or j==0:
                     dp[i][j]=0
-                elif length[i-1]<=j:
-                    dp[i][j]=max(price[i-1]+dp[i][j- length[i-1]],dp[i-1][j])
+                elif i<=j:
+                    dp[i][j]=max(price[i-1]+dp[i][j - i],dp[i-1][j])
                 else:
                     dp[i][j]=dp[i-1][j]
                     
