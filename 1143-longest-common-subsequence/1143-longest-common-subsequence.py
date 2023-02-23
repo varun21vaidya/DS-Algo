5f98f36f09ba7d17bec48cd79624d23434ebe960
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-02-23 23:23:26 +0530

    Time: 745 ms (42.67%), Space: 39.8 MB (51.88%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -15,19 +15,35 @@ class Solution:
     
         # Memoization:
         
-        def solver(x,y):
-            if x==0 or y==0:
-                return 0
+#         def solver(x,y):
+#             if x==0 or y==0:
+#                 return 0
             
-            if dp[x][y]!=-1:
-                return dp[x][y]
+#             if dp[x][y]!=-1:
+#                 return dp[x][y]
             
-            if text1[x-1]==text2[y-1]:
-                dp[x][y]= 1 + solver(x-1,y-1)
-            else:
-                dp[x][y]= max(solver(x,y-1),solver(x-1,y))
+#             if text1[x-1]==text2[y-1]:
+#                 dp[x][y]= 1 + solver(x-1,y-1)
+#             else:
+#                 dp[x][y]= max(solver(x,y-1),solver(x-1,y))
             
-            return dp[x][y]
+#             return dp[x][y]
         
+#         dp=[[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+#         return solver(len(text1),len(text2))
+
+
+        # Tabulation:
         dp=[[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
-        return solver(len(text1),len(text2))
+        x,y=len(text1),len(text2)
+        for i in range(x+1):
+            for j in range(y+1):
+                if i==0 or j==0:
+                    dp[i][j]=0
+                elif text1[i-1]==text2[j-1]:
+                    dp[i][j]=1+ dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return dp[x][y]
+                
+        
