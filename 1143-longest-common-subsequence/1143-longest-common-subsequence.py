9d609295a653cfd5c81805324b64041e4e410174
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-02-24 01:14:15 +0530

    Time: 1027 ms (23.93%), Space: 408.4 MB (5.01%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -28,37 +28,37 @@ class Solution:
     
         # Memoization:
         
-        def solver(x,y):
-            if x==0 or y==0:
-                return ""
+#         def solver(x,y):
+#             if x==0 or y==0:
+#                 return ""
             
-            if dp[x][y]!="":
-                return dp[x][y]
+#             if dp[x][y]!="":
+#                 return dp[x][y]
             
-            if text1[x-1]==text2[y-1]:
-                dp[x][y]= text1[x-1]+solver(x-1,y-1)
-            else:
-                dp[x][y]= max(solver(x,y-1),solver(x-1,y),key=len)
+#             if text1[x-1]==text2[y-1]:
+#                 dp[x][y]= text1[x-1]+solver(x-1,y-1)
+#             else:
+#                 dp[x][y]= max(solver(x,y-1),solver(x-1,y),key=len)
                 
-            return dp[x][y]
+#             return dp[x][y]
         
-        dp=[["" for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
-        x=solver(len(text1),len(text2))
-        print(x)
-        return(len(x))
+#         dp=[["" for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+#         res=solver(len(text1),len(text2))
+#         # print(res)
+#         return(len(res))
 
 
         # Tabulation:
-#         dp=[[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
-#         x,y=len(text1),len(text2)
-#         for i in range(x+1):
-#             for j in range(y+1):
-#                 if i==0 or j==0:
-#                     dp[i][j]=0
-#                 elif text1[i-1]==text2[j-1]:
-#                     dp[i][j]=1+ dp[i-1][j-1]
-#                 else:
-#                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-#         return dp[x][y]
-                
-        
+        dp=[["" for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        x,y=len(text1),len(text2)
+        for i in range(x+1):
+            for j in range(y+1):
+                if i==0 or j==0:
+                    dp[i][j]=""
+                elif text1[i-1]==text2[j-1]:
+                    dp[i][j]=text1[i-1]+ dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1],key=len)
+        res=dp[x][y]
+        print(res)
+        return len(res)
