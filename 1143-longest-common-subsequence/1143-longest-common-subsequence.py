c4df7ecf56212b1a097d8fdc8c8883a6077f5c03
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-02-24 01:11:04 +0530

    Time: 2919 ms (5.00%), Space: 40.3 MB (23.36%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,48 +2,61 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
 #         # recursive
-#         def solver(x,y,text1, text2):
+#         def solver(x,y,temp,res):
 #             if  x==0 or y==0:
-#                 return 0
+#                 return ""
             
 #             if text1[x-1]==text2[y-1]:
-#                 return 1+ solver(x-1, y-1,text1,text2)
+#                 temp=+text1[x-1]+ solver(x-1, y-1,temp,res)
+#                 if len(temp)>len(res[0]): res[0]=temp
+#                 return temp
             
-#             return max( solver(x, y-1,text1,text2), solver(x-1, y,text1,text2))
-    
-#         return solver(len(text1),len(text2), text1,text2) 
+#             a=solver(x, y-1,temp,res)
+#             b=solver(x-1, y,temp,res)
+#             if len(a)>len(b):
+#                 temp=a
+#             else:
+#                 temp=b
+#             if len(temp)>len(res[0]): res[0]=temp
+#             return temp
+            
+#         temp=""
+#         res=[""]
+#         solver(len(text1),len(text2),temp,res) 
+#         print(res)
+#         return len(res[0])
     
         # Memoization:
         
-#         def solver(x,y):
-#             if x==0 or y==0:
-#                 return 0
+        def solver(x,y):
+            if x==0 or y==0:
+                return ""
             
-#             if dp[x][y]!=-1:
-#                 return dp[x][y]
+            if dp[x][y]!="":
+                return dp[x][y]
             
-#             if text1[x-1]==text2[y-1]:
-#                 dp[x][y]= 1 + solver(x-1,y-1)
-#             else:
-#                 dp[x][y]= max(solver(x,y-1),solver(x-1,y))
-            
-#             return dp[x][y]
+            if text1[x-1]==text2[y-1]:
+                dp[x][y]= text1[x-1]+solver(x-1,y-1)
+            else:
+                dp[x][y]= max(solver(x,y-1),solver(x-1,y),key=len)
+                
+            return dp[x][y]
         
-#         dp=[[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
-#         return solver(len(text1),len(text2))
+        dp=[["" for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        return len(solver(len(text1),len(text2)))
 
 
         # Tabulation:
-        dp=[[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
-        x,y=len(text1),len(text2)
-        for i in range(x+1):
-            for j in range(y+1):
-                if i==0 or j==0:
-                    dp[i][j]=0
-                elif text1[i-1]==text2[j-1]:
-                    dp[i][j]=1+ dp[i-1][j-1]
-                else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[x][y]
+#         dp=[[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+#         x,y=len(text1),len(text2)
+#         for i in range(x+1):
+#             for j in range(y+1):
+#                 if i==0 or j==0:
+#                     dp[i][j]=0
+#                 elif text1[i-1]==text2[j-1]:
+#                     dp[i][j]=1+ dp[i-1][j-1]
+#                 else:
+#                     dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+#         return dp[x][y]
                 
         
