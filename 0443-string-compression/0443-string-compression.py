89a4b3473856b27d6ea8d9cfde4e89fbff1f53bb
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-03 01:01:16 +0530

    Time: 65 ms (45.31%), Space: 14 MB (65.01%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,27 +1,46 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
         
-        temp=1
-        s=""
-        if len(chars)==1:
-            return 1
-        for i in range(1, len(chars)):
+        #Brute force # inefficient
+#         temp=1
+#         s=""
+#         if len(chars)==1:
+#             return 1
+#         for i in range(1, len(chars)):
             
-            if chars[i]==chars[i-1]:
-                temp+=1
-            else:
-                s+=chars[i-1]
-                if temp>1:
-                    s+=str(temp)
-                temp=1
-            # print(s,temp,i)
+#             if chars[i]==chars[i-1]:
+#                 temp+=1
+#             else:
+#                 s+=chars[i-1]
+#                 if temp>1:
+#                     s+=str(temp)
+#                 temp=1
+#             # print(s,temp,i)
         
-        s+=chars[i]
-        if temp>1:
-            s+=str(temp)
-        # print(s)
-        chars[:]= list(s)
-        return len(chars)
+#         s+=chars[i]
+#         if temp>1:
+#             s+=str(temp)
+#         chars[:]= list(s)
+#         return len(chars)
                 
-            
-            
+    
+        walker, runner = 0, 0
+        while runner < len(chars):
+
+            chars[walker] = chars[runner]
+            count = 1
+
+            while runner + 1 < len(chars) and chars[runner] == chars[runner+1]:
+                runner += 1
+                count += 1
+
+            if count > 1:
+                for c in str(count):
+                    chars[walker+1] = c
+                    walker += 1
+
+            runner += 1
+            walker += 1
+
+        return walker
+                
