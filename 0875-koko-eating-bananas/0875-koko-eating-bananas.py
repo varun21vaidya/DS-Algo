2adfa6dcff5eed6d5182de8f5f5acf65362dce5f
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-08 23:57:49 +0530

    Time: 469 ms (74.94%), Space: 15.4 MB (62.98%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,11 +1,5 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-
-        # i th pile has piles[i] banana, max h hours are given 
-        # so in this time we have to complete our eating
-        # she can eat at max one pile so max speed could be max(piles)
-        
-
         def check(speed):
             
             return sum(math.ceil(pile / speed) for pile in piles) <= h 
