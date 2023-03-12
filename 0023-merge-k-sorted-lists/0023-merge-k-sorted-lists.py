2c14328aba13e8f79eff9f226543ea0ca026653e
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-13 00:09:10 +0530

    Time: 145 ms (30.36%), Space: 18.7 MB (12.68%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,33 +4,57 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists):
-        if not lists:
-            return None
-        if len(lists) == 1:
-            return lists[0]
-        mid = len(lists) // 2
-        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
-        return self.merge(l, r)
-    
-    def merge(self, l, r):
-        dummy = p = ListNode()
-        while l and r:
-            if l.val < r.val:
-                p.next = l
-                l = l.next
-            else:
-                p.next = r
-                r = r.next
-            p = p.next
-        p.next = l or r
-        return dummy.next
-    
-    def merge1(self, l, r):
-        if not l or not r:
-            return l or r
-        if l.val< r.val:
-            l.next = self.merge(l.next, r)
-            return l
-        r.next = self.merge(l, r.next)
-        return r
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def mergesort(arr):
+            if len(arr)<=1:
+                return arr
+            mid= len(arr)//2
+            x= mergesort(arr[:mid])
+            y= mergesort(arr[mid:])
+
+            return merge(x,y)
+        
+        def merge(arr1, arr2):
+            m=len(arr1)
+            n= len(arr2)
+            arr=[-1]*(m+n)
+            i,j,k=0,0,0
+            while i<m and j<n:
+                if arr1[i]<=arr2[j]:
+                    arr[k]=arr1[i]
+                    i+=1
+                else:
+                    arr[k]=arr2[j]
+                    j+=1
+                k+=1
+            
+            while j<n:
+                arr[k]=arr2[j]
+                j+=1
+                k+=1
+            while i<m:
+                arr[k]=arr1[i]
+                i+=1
+                k+=1
+            
+            return arr
+        
+        def lltoarr(lists):
+            res=[]
+            for i in lists:
+                while i:
+                    res.append(i.val)
+                    i=i.next
+            res= mergesort(res)
+            return arrtoll(res)
+        
+        def arrtoll(arr):
+            x= head= ListNode()
+            for i in arr:
+                x.next=ListNode(i)
+                x=x.next
+            return head.next
+        
+            
+        return lltoarr(lists)
+        
