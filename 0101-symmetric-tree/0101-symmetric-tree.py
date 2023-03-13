85938939d0ee8586c6a6525b820c7d237734282b
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-13 23:55:13 +0530

    Time: 39 ms (43.16%), Space: 14 MB (14.51%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,7 +6,13 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
+
+                    # Mirror property is    left == right and right == left
+            # pre-order traversal on root->left subtree, (root, left, right)
+            # modified pre-order traversal on root->right subtree, (root, right, left) 
+            # compare the node val's if they are the same 
+            # Do both traversals at the same time
+            # if left is null or right is null, then both sides must match and return true (base case)
         def solver(left, right):
             # if one of the left or right is null other has to be null for symmetric
             if not left or not right:
