b570aef9ab7e2d8558f1f6ea7ab92ef7823b815c
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-14 23:57:23 +0530

    Time: 32 ms (70.48%), Space: 13.9 MB (10.80%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,24 +6,34 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def solver(temp, root):
-            nonlocal summ
-            if root and not root.left and not root.right:
-                path=int("".join(temp)+str(root.val))
-                # print("path",path)
-                summ+=path
-                return 
-            if root:
-                temp.append(str(root.val))
-                # print(temp)
-                # print("to left")
-                solver(temp,root.left)
-                # print(temp)
-                # print("to right")
-                solver(temp,root.right)
-                if temp: temp.pop()
+#         def solver(temp, root):
+#             nonlocal summ
+#             if root and not root.left and not root.right:
+#                 path=int("".join(temp)+str(root.val))
+#                 # print("path",path)
+#                 summ+=path
+#                 return 
+#             if root:
+#                 temp.append(str(root.val))
+#                 # print(temp)
+#                 # print("to left")
+#                 solver(temp,root.left)
+#                 # print(temp)
+#                 # print("to right")
+#                 solver(temp,root.right)
+#                 if temp: temp.pop()
                     
-        summ=0
-        solver([], root)
-        return summ
+#         summ=0
+#         solver([], root)
+#         return summ
         
+    
+        def solver(root,summ):
+            if not root:
+                return 0
+            summ=summ*10+root.val
+            if not root.left and not root.right:
+                return summ
+            return solver(root.left,summ)+ solver(root.right,summ)
+        
+        return solver(root,0)
