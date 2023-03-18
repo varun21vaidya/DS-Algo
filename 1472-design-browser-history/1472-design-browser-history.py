49ea21c9e7808951e905f5f6a9bf76acc9fcf5e8
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-19 00:28:02 +0530

    Time: 276 ms (56.72%), Space: 16.9 MB (10.57%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -28,39 +28,74 @@
 #             steps-=1 
 #         return self.store[-1]
         
+
+# Vector / ArrayList / Python List
+# class BrowserHistory:
+
+#     def __init__(self, homepage: str):
+#         self.store=[homepage] # stores incoming urls
+#         self.ind=0
+
+#     def visit(self, url: str) -> None:
+#         # print(url,"added",self.store)
+#         if self.ind<len(self.store)-1:
+#             self.store=self.store[:self.ind+1]
+#         self.store.append(url)
+#         self.ind+=1
+
+
+#     def back(self, steps: int) -> str:
+#         # print("go",steps,"steps backward", self.store)
+#         if steps>self.ind:
+#             self.ind=0
+#         else:
+#             self.ind=self.ind-steps
+#         return self.store[self.ind]
+            
+
+
+#     def forward(self, steps: int) -> str:
+#         # print("go",steps,"steps forward",self.store)
+#         if len(self.store)-1-self.ind>=steps:
+#             self.ind=self.ind+steps
+#         else:
+#             self.ind=len(self.store)-1
+#         return self.store[self.ind]
+
+class Node:
+    def __init__(self,value):
+        self.data=value
+        self.next=None
+        self.prev=None
+
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.store=[homepage] # stores incoming urls
-        self.ind=0
+        self.dll=Node(homepage)
 
     def visit(self, url: str) -> None:
         # print(url,"added",self.store)
-        if self.ind<len(self.store)-1:
-            self.store=self.store[:self.ind+1]
-        self.store.append(url)
-        self.ind+=1
+        self.newNode= Node(url)
+        self.dll.next=self.newNode
+        self.newNode.prev=self.dll
+        self.dll=self.dll.next
 
 
     def back(self, steps: int) -> str:
         # print("go",steps,"steps backward", self.store)
-        if steps>self.ind:
-            self.ind=0
-        else:
-            self.ind=self.ind-steps
-        return self.store[self.ind]
-            
+        while steps and self.dll.prev:
+            self.dll=self.dll.prev
+            steps-=1
+        return self.dll.data
 
 
     def forward(self, steps: int) -> str:
         # print("go",steps,"steps forward",self.store)
-        if len(self.store)-1-self.ind>=steps:
-            self.ind=self.ind+steps
-        else:
-            self.ind=len(self.store)-1
-        return self.store[self.ind]
-
-
+        while steps and self.dll.next:
+            self.dll=self.dll.next
+            steps-=1
+        return self.dll.data
+    
 
 # Your BrowserHistory object will be instantiated and called as such:
 # obj = BrowserHistory(homepage)
