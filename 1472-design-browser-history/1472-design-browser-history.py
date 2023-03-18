f3142cdaa968cceb2c11cffde239cbddb9e323c1
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-19 00:19:16 +0530

    Time: 213 ms (92.45%), Space: 16.5 MB (85.99%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,30 +1,65 @@
+
+# Two Stack Approach:
+
+# class BrowserHistory:
+
+#     def __init__(self, homepage: str):
+#         self.store=[homepage] # stores incoming urls
+#         self.future=[]  # does operations like forward and backword and returns output
+
+#     def visit(self, url: str) -> None:
+#         self.store.append(url)
+#         # print(url,"added",self.store)
+#         self.future=[]
+
+#     def back(self, steps: int) -> str:
+#         # print("go",steps,"steps backward", self.store)
+#         while steps and len(self.store)>1:
+#             self.future.append(self.store.pop())
+#             steps-=1
+        
+#         return self.store[-1]
+        
+
+#     def forward(self, steps: int) -> str:
+#         # print("go",steps,"steps forward",self.store)
+#         while steps and self.future:
+#             self.store.append(self.future.pop())
+#             steps-=1 
+#         return self.store[-1]
+        
 class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.store=[homepage] # stores incoming urls
-        self.future=[]  # does operations like forward and backword and returns output
+        self.ind=0
 
     def visit(self, url: str) -> None:
-        self.store.append(url)
         # print(url,"added",self.store)
-        self.future=[]
+        if self.ind<len(self.store)-1:
+            self.store=self.store[:self.ind+1]
+        self.store.append(url)
+        self.ind+=1
+
 
     def back(self, steps: int) -> str:
         # print("go",steps,"steps backward", self.store)
-        while steps and len(self.store)>1:
-            self.future.append(self.store.pop())
-            steps-=1
-        
-        return self.store[-1]
-        
+        if steps>self.ind:
+            self.ind=0
+        else:
+            self.ind=self.ind-steps
+        return self.store[self.ind]
+            
+
 
     def forward(self, steps: int) -> str:
         # print("go",steps,"steps forward",self.store)
-        while steps and self.future:
-            self.store.append(self.future.pop())
-            steps-=1 
-        return self.store[-1]
-        
+        if len(self.store)-1-self.ind>=steps:
+            self.ind=self.ind+steps
+        else:
+            self.ind=len(self.store)-1
+        return self.store[self.ind]
+
 
 
 # Your BrowserHistory object will be instantiated and called as such:
