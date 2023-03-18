309f46a8cc221fffd6fe2e0a55ba21ed91306fd1
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-19 00:01:31 +0530

    Time: 289 ms (41.58%), Space: 16.7 MB (37.23%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -2,17 +2,17 @@ class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.store=[homepage] # stores incoming urls
-        self.show=[]  # does operations like forward and backword and returns output
+        self.future=[]  # does operations like forward and backword and returns output
 
     def visit(self, url: str) -> None:
         self.store.append(url)
         # print(url,"added",self.store)
-        self.show=[]
+        self.future=[]
 
     def back(self, steps: int) -> str:
         # print("go",steps,"steps backward", self.store)
         while steps and len(self.store)>1:
-            self.show.append(self.store.pop())
+            self.future.append(self.store.pop())
             steps-=1
         
         return self.store[-1]
@@ -20,8 +20,8 @@ class BrowserHistory:
 
     def forward(self, steps: int) -> str:
         # print("go",steps,"steps forward",self.store)
-        while steps and self.show:
-            self.store.append(self.show.pop())
+        while steps and self.future:
+            self.store.append(self.future.pop())
             steps-=1 
         return self.store[-1]
         
