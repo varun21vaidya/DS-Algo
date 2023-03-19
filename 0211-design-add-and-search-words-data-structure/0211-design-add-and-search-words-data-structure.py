3132eac761808980be8b74b633a332e5aed479c6
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-19 23:50:19 +0530

    Time: 3187 ms (93.79%), Space: 21.9 MB (98.81%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,23 +1,46 @@
+# class WordDictionary:
+#     def __init__(self):
+#         self.trie = {}
+
+#     def addWord(self, word: str) -> None:
+#         node = self.trie
+#         for c in word:
+#             node = node.setdefault(c, {})
+#         node['$'] = True
+
+#     def search(self, word: str) -> bool:
+#         return self.searchNode(self.trie, word)
+
+#     def searchNode(self, node, word: str) -> bool:
+#         for i, c in enumerate(word):
+#             if c == '.':
+#                 return any(self.searchNode(node[w], word[i+1:]) for w in node if w != '$')
+#             if c not in node: return False
+#             node = node[c]
+#         return '$' in node
+
+import re
 class WordDictionary:
     def __init__(self):
-        self.trie = {}
-
+        self.d = {}
+        
     def addWord(self, word: str) -> None:
-        node = self.trie
-        for c in word:
-            node = node.setdefault(c, {})
-        node['$'] = True
-
+        self.d[word]  = 1
+        self.d[len(word)] = self.d.get(len(word),[]) + [word]
+    
     def search(self, word: str) -> bool:
-        return self.searchNode(self.trie, word)
-
-    def searchNode(self, node, word: str) -> bool:
-        for i, c in enumerate(word):
-            if c == '.':
-                return any(self.searchNode(node[w], word[i+1:]) for w in node if w != '$')
-            if c not in node: return False
-            node = node[c]
-        return '$' in node
+        if '.' not in word:
+            if self.d.get(word) != None:
+                return True
+            else:
+                return False
+        else:
+            if self.d.get(len(word)) != None:
+                lis = self.d[len(word)]
+                for string in lis:
+                    if re.match(word,string):
+                        return True
+            return False
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
