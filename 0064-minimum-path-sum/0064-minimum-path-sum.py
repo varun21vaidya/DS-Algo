7d5f3876437a29fb7d5543c165cd4e72926f7f96
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-27 13:57:42 +0530

    Time: 94 ms (87.57%), Space: 15.7 MB (80.67%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -31,28 +31,43 @@ class Solution:
 #         return ans
                 
     
+#         m=len(grid)
+#         n=len(grid[0])
+
+#         def solver(x,y,summ):
+
+#             if x>=m or y>=n or x<0 or y<0:
+#                 return float('inf')
+
+#             if x==m-1 and y==n-1:
+#                 return grid[x][y]
+
+#             if dp[x][y]!=-1:
+#                 return dp[x][y]
+
+
+#             dp[x][y]=grid[x][y]+ min(solver(x+1,y,summ),solver(x,y+1,summ))
+
+#             return dp[x][y]
+
+
+#         if m==1 and n==1:
+#             return grid[0][0]
+#         dp=[[-1]*(n) for i in range(m)]
+#         solver(0,0,0)
+#         return dp[0][0]
+
         m=len(grid)
         n=len(grid[0])
-
-        def solver(x,y,summ):
-
-            if x>=m or y>=n or x<0 or y<0:
-                return float('inf')
-
-            if x==m-1 and y==n-1:
-                return grid[x][y]
-
-            if dp[x][y]!=-1:
-                return dp[x][y]
-
-
-            dp[x][y]=grid[x][y]+ min(solver(x+1,y,summ),solver(x,y+1,summ))
-
-            return dp[x][y]
-
-
-        if m==1 and n==1:
-            return grid[0][0]
-        dp=[[-1]*(n) for i in range(m)]
-        solver(0,0,0)
-        return dp[0][0]
+        for i in range(m):
+            for j in range(n):
+                if not i and not j:
+                    continue
+                if i==0:
+                    grid[i][j]+=grid[i][j-1]
+                elif j==0:
+                    grid[i][j]+=grid[i-1][j]
+                else:
+                    grid[i][j]+=min(grid[i-1][j],grid[i][j-1])
+                    
+        return grid[-1][-1]
