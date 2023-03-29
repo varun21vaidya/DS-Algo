6b7c5aaa267bbc69611fa7c8f1f10df97efcbea3
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-03-30 00:23:24 +0530

    Time: 40 ms (81.65%), Space: 13.9 MB (77.85%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,22 +1,32 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         # greedy
-        satisfaction.sort()
-        maxx=float('-inf')
+#         satisfaction.sort()
+#         maxx=float('-inf')
         
-        def calc(satisfaction):
-            summ=0
-            for i in range(len(satisfaction)):
-                summ+=(i+1)*satisfaction[i]
-            return summ
-        maxx=max(maxx,calc(satisfaction))
-        # print(maxx)
-        while satisfaction:
-            satisfaction.pop(0)
-            # print(satisfaction)
-            x=calc(satisfaction)
-            # print(x)
-            maxx=max(maxx,x)
+#         def calc(satisfaction):
+#             summ=0
+#             for i in range(len(satisfaction)):
+#                 summ+=(i+1)*satisfaction[i]
+#             return summ
+#         maxx=max(maxx,calc(satisfaction))
+#         while satisfaction:
+#             satisfaction.pop(0)
+#             x=calc(satisfaction)
+#             maxx=max(maxx,x)
         
+#         return maxx
+
+        maxx=0
+        summ=0
+        satisfaction.sort(reverse=True)
+        for i in range(len(satisfaction)):
+
+            summ+=satisfaction[i]
+            if summ<0:
+                return maxx
+            maxx+= summ
+            
         return maxx
+        
             
