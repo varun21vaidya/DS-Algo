afb0224c8e749f42fe7f4e3f17586d9a40a69b43
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-04-02 23:45:01 +0530

    Time: 338 ms (36.11%), Space: 21.9 MB (25.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -18,12 +18,29 @@ class Solution:
                         vis.add((row,col))
                         q.append((row,col))
             
-        
+            
+            
+        def dfs(i,j,vis):
+            drow=[-1,0,1,0]
+            dcol=[0,1,0,-1]
+            m=len(grid)
+            n=len(grid[0])
+            vis.add((i,j))
+            
+            for d in range(4):
+                row,col=drow[d]+i,dcol[d]+j
+                if row>=0 and row<m and col>=0 and col<n and grid[row][col]=="1" and (row,col) not in vis:
+                    vis.add((row,col))
+                    dfs(row,col,vis)
+            
+            
+            
+            
         vis=set()
         count=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]=="1" and (i,j) not in vis:
-                        bfs(i,j,vis)
+                        dfs(i,j,vis)
                         count+=1
         return count
