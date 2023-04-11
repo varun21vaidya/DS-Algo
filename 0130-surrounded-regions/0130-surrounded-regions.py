e9d4944009444e717679e92ee2492fb442b3c08a
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-04-12 00:30:49 +0530

    Time: 140 ms (60.65%), Space: 16.1 MB (35.36%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,6 +3,11 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+    # now first we need to understand the problem, which states that we need to find
+    # the "O"s which are fully surrounded by "X" now it also means we need to skip
+    # those "O"s which are on boundry, so from our Jugaad we will find Ulta Solution
+    # we will find the "O"s on the boundry store them in visited and then convert those
+    # "O"s which are not in visited set.
         
         drow=[-1,0,1,0]
         dcol=[0,1,0,-1]
@@ -10,6 +15,9 @@ class Solution:
         n=len(board[0])
         vis=set()
         
+
+        # dfs which traverses from boundry and marks those Os which are adjecent to boundry
+        # ie these are not eligible to converted
         def dfs(i,j,vis):
             vis.add((i,j))
             
@@ -19,14 +27,14 @@ class Solution:
                 if row>=0 and col>=0 and row<m and col<n and (row,col) not in vis and board[row][col]=="O":
                     dfs(row,col,vis)
                     
-                    
+        # traveres throgh matrix but only fetch boundry elements and finds "O"s on boundry and uses dfs on them            
         for i in range(m):
             for j in range(n):
                 if i==0 or j==0 or i==m-1 or j==n-1:
                     if board[i][j]=="O":
                         dfs(i,j,vis)
                         
-        
+        # when we got all non eligible marked with vis set, again traverse and convert those Os which are not in vis
         for r in range(m):
             for c in range(n):
                 if board[r][c]=="O" and (r,c) not in vis:
