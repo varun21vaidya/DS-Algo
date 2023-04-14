6c21e57946fa7ae1dbb2c7f8bce593f0a800ef72
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-04-14 12:44:51 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
@@ -1,36 +1,64 @@
 #User function Template for python3
 
-
+from collections import deque
 class Solution:
     
     #Function to detect cycle in a directed graph.
     def isCyclic(self, V, adj):
         # code here
-    
-        def dfs(start):
-            vis[start]=2 # as its visited and path is continued
-            
-            for item in adj[start]:
-                if vis[item]==0:
-                    if dfs(item): return True
-                    
-                elif vis[item]==2:
-                    return True
-                
-            vis[start]=1
-            return False
-    
-        # for multiple components
-        vis= [0]*V
-        # we will use marking like 
-        # for just visited but not on same path = 1
-        # for visited and on same path = 2
         
+        # BFS Using Kahn's Algorithm:
+        indegree=[0]*V
+        for v in range(V):
+            for item in adj[v]:
+                indegree[item]+=1
+        # print(indegree)
+        q=deque()
+        res=[]
         for i in range(V):
-            if vis[i]==0:
-                if dfs(i): return True
+            if indegree[i]==0:
+                q.append(i)
+                res.append(i)
+                
+        while q:
+            node=q.popleft()
+            for item in adj[node]:
+                indegree[item]-=1
+                if indegree[item]==0:
+                    q.append(item)
+                    res.append(item)
+
+        # now we know that topo sort or Kahn's algo only works for Directed Acyclic Graph
+        # so result would not have size of V if its cycle 
+        return len(res)!=V
+                
             
-        return False
+        
+    
+        # def dfs(start):
+        #     vis[start]=2 # as its visited and path is continued
+            
+        #     for item in adj[start]:
+        #         if vis[item]==0:
+        #             if dfs(item): return True
+                    
+        #         elif vis[item]==2:
+        #             return True
+                
+        #     vis[start]=1
+        #     return False
+    
+        # # for multiple components
+        # vis= [0]*V
+        # # we will use marking like 
+        # # for just visited but not on same path = 1
+        # # for visited and on same path = 2
+        
+        # for i in range(V):
+        #     if vis[i]==0:
+        #         if dfs(i): return True
+            
+        # return False
         
     
 #{ 
