f1b249de8f6333045c51430395021e4975d1d681
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-04-16 20:07:07 +0530

    Added solution - LeetHub

--- Alien Dictionary - GFG/alien-dictionary.py
+++ Alien Dictionary - GFG/alien-dictionary.py
@@ -3,57 +3,74 @@ from collections import defaultdict,deque
 class Solution:
     def findOrder(self,alien_dict, N, K):
     # code here
-        alph = dict()
+        # if you observe that we have to decode the alien dictionary
+        # by comparing with our letters, first K letters of our alphabetes are to be used
+        # now when you observe [baa, abcd, abca ] you can tell that b should be before a , d before a like this pairs
+        # so it seems familiar with topological sort and these pairs are indexes and K letters are vertices
+        # So first create a string of our own alphabets
+        # then we will create a hashmap with K letters and their index, which will be easy to operate graph
+
+
+        # now if its topological sort, we will need to create an adjecent list
+        # so we will traverse in this alien dictionary and compare the words
+        # now when the word letters are not same then first word letter will have higher priority in sort
+        # eg for abcd and abca, d and a are diff but abcd is earlier in list so d-->a like this create adj list
+        # then you can use dfs or bfs.
+
         alphabets="abcdefghijklmnopqrstuvwxyz"
-        for l in range(K):
-            alph[alphabets[l]]=l
-        # print(alph)
-        
+        hashmap=dict()
         adj=[[] for _ in range(K)]
+
+        for i in range(K):
+            hashmap[alphabets[i]]=i
         
-        # create adj list
-        for i in range(1,N):
-            letter, maxlen=0,min(len(alien_dict[i-1]),len(alien_dict[i]))
-            
+        # print(hashmap)
+        
+        for word in range(1,N):
+            letter, maxlen= 0, min(len(alien_dict[word-1]),len(alien_dict[word]))
+
             while letter<maxlen:
-                if alien_dict[i-1][letter]!=alien_dict[i][letter]:
-                    # print(alien_dict[i-1][letter],"-->",alien_dict[i][letter])
-                    adj[alph[alien_dict[i-1][letter]]].append(alph[alien_dict[i][letter]])
+                if alien_dict[word-1][letter]!=alien_dict[word][letter]:
+                    adj[hashmap[alien_dict[word-1][letter]]].append(hashmap[alien_dict[word][letter]])
                     break
                 letter+=1
-            
+        
         # print(adj)
-            
-        
-        bfs=[]
-        indegree=[0]*K
+
+        bfs=[] # list to store the topological sort of indexs
         q=deque()
+
+        indegree=[0]*K
         
-        # populate indegree
+        # populate the indegree
         for v in range(K):
             for item in adj[v]:
                 indegree[item]+=1
         
+        # get zero indegree nodes
         for v in range(K):
             if indegree[v]==0:
                 q.append(v)
         
-        # print(indegree)
+        # start bfs loop
         while q:
-            
             node = q.popleft()
             bfs.append(node)
             for item in adj[node]:
                 indegree[item]-=1
+
                 if indegree[item]==0:
                     q.append(item)
-                    
+
         # print(bfs)
-        ans=[]
+
+        # convert the index into letters again
+        res=[]
         for i in range(K):
-            ans.append(alphabets[bfs[i]])
-        # print(ans)
-        return ans
+            res.append(alphabets[bfs[i]])
+            
+        # print(res)
+        return res
 
 
 #{ 
