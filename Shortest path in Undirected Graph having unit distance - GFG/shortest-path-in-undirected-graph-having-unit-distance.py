45ed95098a97e48e84828a9c3076239867104a8f
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-04-17 13:21:36 +0530

    Added solution - LeetHub

--- Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.py
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.py
@@ -5,38 +5,61 @@ class Solution:
         # code here
         
         # shortest path
-        # create adj list from edges
-        # topo sort using bfs / dfs
-        # if dfs pop each node and traverse though each neighbour and get min distance
+        # in bfs pop each node and traverse though each neighbour and get min distance
         # return the array of min dist
         
-        adj=[[]for _ in range(n)]
+        adj=[[] for _ in range(n)]
 
-        for v,item in edges:
-            adj[v].append([item,1])
-            adj[item].append([v,1])
-        # print(adj)
+        for v,it in edges:
+            adj[it].append(v)
+            adj[v].append(it)
+
+        # bfs
+        # q= deque([src])
+        # dist=[float('inf')]*n
+        # dist[src]=0
+
+        # while q:
+        #     node = q.popleft()
+        #     for item in adj[node]:
+                
+        #         if dist[item]>dist[node]+1:
+        #             dist[item]=dist[node]+1
+        #             q.append(item)
+
+        # for i in range(n):
+        #     if dist[i]==float('inf'):
+        #         dist[i]=-1
+
+        # return dist
         
-        q=deque()
-        q.append(src)
+        # dfs
+        def dfs(node):
+            vis[node]=1
+            
+            for item in adj[node]:
+                if dist[item]>dist[node]+1:
+                    dist[item]=dist[node]+1
+                if vis[item]==0:
+                    dfs(item)
+        
+            vis[node]=0
+        
+        vis=[0]*n
         dist=[float('inf')]*n
         dist[src]=0
+        for i in range(n):
+            if vis[i]==0:
+                dfs(i)
         
-        while q:
-            
-            v = q.popleft()
-             
-            for item,wt in adj[v]:
-                # dist[item]=min(dist[v]+wt, dist[item])
-                if dist[item]>dist[v]+1:
-                    dist[item]=dist[v]+1
-                    q.append(item)
-                
-        for d in range(n):
-            if dist[d]==float('inf'):
-                dist[d]=-1
+        for i in range(n):
+            if dist[i]==float('inf'):
+                dist[i]=-1
         
         return dist
+        
+        
+
 
 #{ 
  # Driver Code Starts
