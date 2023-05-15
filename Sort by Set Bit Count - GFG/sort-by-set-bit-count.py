235724aa08d45b75bfb24ddb58978510d7e47bd1
Varun Vaidya <65745841+varun21vaidya@users.noreply.github.com>
2023-05-15 20:12:28 +0530

    Added solution - LeetHub

--- Sort by Set Bit Count - GFG/sort-by-set-bit-count.py
+++ Sort by Set Bit Count - GFG/sort-by-set-bit-count.py
@@ -21,52 +21,12 @@ class Solution:
         temp= []
         for elem in arr:
             temp.append([elem,countSet(elem)])
-        
-        # print(temp)
-        
-        # n log n algo
-        # def sort(array):
-        #     if len(array)>1:
-        #         mid= len(array)//2
-        #         left= sort(array[:mid])
-        #         right= sort(array[mid:])
-            
-        #         return mergesort(left, right)
-                
-        #     else:
-        #         return array
-            
-        # def mergesort(left, right):
-        #     a,b,m,n = 0,0,len(left), len(right)
-        #     merge=[]
-            
-        #     while a<m and b<n:
-        #         if left[a]<right[b]:
-        #             merge.append(left[a])
-        #             a+=1
-        #         else:
-        #             merge.append(right[b])
-        #             b+=1
-            
-        #     merge.extend(left[a:])
-        #     merge.extend(right[b:])
-            
-        #     return merge
-            # sort(temp.keys()) sorted(temp.values())
-        # sortedsets= sort(list(temp.values())) 
-        # sortedarray= []
-        
+
         sortedarray = sorted(temp,key = lambda k : k[1], reverse=True)
         arr[:] = [pair[0] for pair in sortedarray]
         
         return arr
-        
-        # for sortset in sortedsets:
-        #     sortedarray.append(temp[sortset])
-        
-        # return sortedarray[::-1]
-            
-        
+
 
 #{ 
  # Driver Code Starts
